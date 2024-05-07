6608b208a4dae7ca61e69f23e519e50672cc8e30
quxin024 <quixn9@huawei.com>
2024-05-07 11:40:23 +0800

    implement sync solution

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/SchedulerDelegateCAPI.h
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/SchedulerDelegateCAPI.h
@@ -77,7 +77,16 @@ class SchedulerDelegateCAPI : public facebook::react::SchedulerDelegate {
 
   void schedulerDidRequestPreliminaryViewAllocation(
       facebook::react::SurfaceId surfaceId,
-      const facebook::react::ShadowNode& shadowNode) override {}
+      const facebook::react::ShadowNode& shadowNode) override {
+    auto componentInstance = m_componentInstanceFactory->create(
+        shadowNode.getTag(), shadowNode.getComponentHandle(), shadowNode.getComponentName());
+    if (componentInstance == nullptr) {
+      LOG(INFO) << "Couldn't create CppComponentInstance for: "
+                << shadowNode.getComponentName();
+    } else {
+      m_componentInstanceRegistry->insert(componentInstance);
+    }
+  }
 
   void schedulerDidDispatchCommand(
       const facebook::react::ShadowView& shadowView,
@@ -140,15 +149,8 @@ class SchedulerDelegateCAPI : public facebook::react::SchedulerDelegate {
     switch (mutation.type) {
       case facebook::react::ShadowViewMutation::Create: {
         auto newChild = mutation.newChildShadowView;
-        auto componentInstance = m_componentInstanceFactory->create(
-            newChild.tag, newChild.componentHandle, newChild.componentName);
-        if (componentInstance != nullptr) {
-          updateComponentWithShadowView(componentInstance, newChild);
-          m_componentInstanceRegistry->insert(componentInstance);
-        } else {
-          LOG(INFO) << "Couldn't create CppComponentInstance for: "
-                    << newChild.componentName;
-        }
+        auto componentInstance = m_componentInstanceRegistry->findByTag(newChild.tag);
+        updateComponentWithShadowView(componentInstance, newChild);
         break;
       }
       case facebook::react::ShadowViewMutation::Delete: {
