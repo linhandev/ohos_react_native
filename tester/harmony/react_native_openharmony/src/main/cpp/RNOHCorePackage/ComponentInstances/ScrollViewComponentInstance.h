93ece1eadced69cac1f2a7af78a37958b2ad65e9
quxin024 <quixn9@huawei.com>
2024-05-24 00:17:49 +0800

    Revert "Fix contentOffset not set at the first render"
    Signed-off-by: hyh <huangyouhua2@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/ComponentInstances/ScrollViewComponentInstance.h
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/ComponentInstances/ScrollViewComponentInstance.h
@@ -104,7 +104,6 @@ class ScrollViewComponentInstance
   void onScrollStart() override;
   void onScrollStop() override;
   float onScrollFrameBegin(float offset, int32_t scrollState) override;
-  void onAppear() override;
 
   void finalizeUpdates() override;
 
