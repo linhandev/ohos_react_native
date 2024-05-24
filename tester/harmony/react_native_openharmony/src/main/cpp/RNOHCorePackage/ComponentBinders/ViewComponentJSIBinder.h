5147cc5d0b24c0315f6898164ab3484ac087b085
Patryk Klatka <patryk.klatka@swmansion.com>
2024-05-24 19:06:46 +0200

    ViewComponentJSIBinder: set ViewEvents props
    ## Summary
    In [ViewShadowNode.cpp](https://github.com/facebook/react-native/blob/v0.72.0/packages/react-native/ReactCommon/react/renderer/components/view/ViewShadowNode.cpp#L52), there is an `events` bitset which sets specific bits for given `ViewEvents` props. Previously, this bitset was always empty due to the fact that the ViewEvents props were not being recognized as native props. This was causing unnecessary collapses for the Views that had Responder Lifecycle callbacks set.   
    
    ## Changes
    - handle `ViewEvents` props in ViewComponentJSIBinder
    
    ## Test Plan
    Tested whether corresponding bits are correctly set for the given ViewEvents prop in ViewShadowNode.cpp.
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    ## Issue Links
    Closes: #1061
    
    See merge request rnoh/react-native-harmony!1213
    
    Signed-off-by: huangyouhua <1356795413@qq.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/ComponentBinders/ViewComponentJSIBinder.h
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/ComponentBinders/ViewComponentJSIBinder.h
@@ -5,6 +5,51 @@
 namespace rnoh {
 class ViewComponentJSIBinder : public BaseComponentJSIBinder {
  protected:
+  facebook::jsi::Object createNativeProps(facebook::jsi::Runtime& rt) override {
+    auto nativeProps = BaseComponentJSIBinder::createNativeProps(rt);
+
+    // #region ViewEvents
+
+    // Pointer events
+    nativeProps.setProperty(rt, "onPointerEnter", "boolean");
+    nativeProps.setProperty(rt, "onPointerMove", "boolean");
+    nativeProps.setProperty(rt, "onPointerLeave", "boolean");
+
+    // PanResponder callbacks
+    nativeProps.setProperty(rt, "onMoveShouldSetResponder", "boolean");
+    nativeProps.setProperty(rt, "onMoveShouldSetResponderCapture", "boolean");
+    nativeProps.setProperty(rt, "onStartShouldSetResponder", "boolean");
+    nativeProps.setProperty(rt, "onStartShouldSetResponderCapture", "boolean");
+    nativeProps.setProperty(rt, "onResponderGrant", "boolean");
+    nativeProps.setProperty(rt, "onResponderReject", "boolean");
+    nativeProps.setProperty(rt, "onResponderStart", "boolean");
+    nativeProps.setProperty(rt, "onResponderEnd", "boolean");
+    nativeProps.setProperty(rt, "onResponderRelease", "boolean");
+    nativeProps.setProperty(rt, "onResponderMove", "boolean");
+    nativeProps.setProperty(rt, "onResponderTerminate", "boolean");
+    nativeProps.setProperty(rt, "onResponderTerminationRequest", "boolean");
+    nativeProps.setProperty(rt, "onShouldBlockNativeResponder", "boolean");
+
+    // Touch events
+    nativeProps.setProperty(rt, "onTouchStart", "boolean");
+    nativeProps.setProperty(rt, "onTouchMove", "boolean");
+    nativeProps.setProperty(rt, "onTouchEnd", "boolean");
+    nativeProps.setProperty(rt, "onTouchCancel", "boolean");
+
+    // W3C Pointer Events
+    nativeProps.setProperty(rt, "onPointerEnter", "boolean");
+    nativeProps.setProperty(rt, "onPointerLeave", "boolean");
+    nativeProps.setProperty(rt, "onPointerMove", "boolean");
+    nativeProps.setProperty(rt, "onPointerOver", "boolean");
+    nativeProps.setProperty(rt, "onPointerOut", "boolean");
+    nativeProps.setProperty(rt, "onPointerEnterCapture", "boolean");
+    nativeProps.setProperty(rt, "onPointerLeaveCapture", "boolean");
+    nativeProps.setProperty(rt, "onPointerMoveCapture", "boolean");
+
+    // #endregion
+
+    return nativeProps;
+  }
   facebook::jsi::Object createBubblingEventTypes(
       facebook::jsi::Runtime& rt) override {
     facebook::jsi::Object events(rt);
