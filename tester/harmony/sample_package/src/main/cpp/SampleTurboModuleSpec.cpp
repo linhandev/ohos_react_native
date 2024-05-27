e7c64384131f8754170bdbc7f3910c34c3495bf2
tancui1 <tancui1@huawei.com>
2024-05-27 10:37:30 +0200

    chore: add more methods to SampleTurboModule
    ## Summary
    This PR adds remaining SampleTurboModule's methods specified by the SampleTurboModule used on Android and iOS to allow.
    
    ## Test Plan
    Evaluate official RN tester app's turbo module examples.
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    See merge request rnoh/react-native-harmony!1219
    
    Signed-off-by: huangyouhua <1356795413@qq.com>

--- tester/harmony/sample_package/src/main/cpp/SampleTurboModuleSpec.cpp
+++ tester/harmony/sample_package/src/main/cpp/SampleTurboModuleSpec.cpp
@@ -5,89 +5,6 @@
 using namespace rnoh;
 using namespace facebook;
 
-static jsi::Value __hostFunction_NativeSampleTurboModuleSpecJSI_voidFunc(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return static_cast<ArkTSTurboModule&>(turboModule)
-      .call(rt, "voidFunc", args, count);
-}
-
-static jsi::Value __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getBool(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return jsi::Value(static_cast<ArkTSTurboModule&>(turboModule)
-                        .call(rt, "getBool", args, count));
-}
-
-static jsi::Value __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getNull(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return jsi::Value(static_cast<ArkTSTurboModule&>(turboModule)
-                        .call(rt, "getNull", args, count));
-}
-
-static jsi::Value __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getString(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return jsi::Value(static_cast<ArkTSTurboModule&>(turboModule)
-                        .call(rt, "getString", args, count));
-}
-
-static jsi::Value __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getObject(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return jsi::Value(static_cast<ArkTSTurboModule&>(turboModule)
-                        .call(rt, "getObject", args, count));
-}
-
-static jsi::Value __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getArray(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return jsi::Value(static_cast<ArkTSTurboModule&>(turboModule)
-                        .call(rt, "getArray", args, count));
-}
-
-static jsi::Value
-__hostFunction_NativeSampleTurboCxxModuleSpecJSI_registerFunction(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return jsi::Value(static_cast<ArkTSTurboModule&>(turboModule)
-                        .call(rt, "registerFunction", args, count));
-}
-
-static jsi::Value __hostFunction_NativeSampleTurboCxxModuleSpecJSI_doAsyncJob(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return jsi::Value(static_cast<ArkTSTurboModule&>(turboModule)
-                        .callAsync(rt, "doAsyncJob", args, count));
-}
-
-static jsi::Value
-__hostFunction_NativeSampleTurboCxxModuleSpecJSI_getPromisedArray(
-    jsi::Runtime& rt,
-    react::TurboModule& turboModule,
-    const jsi::Value* args,
-    size_t count) {
-  return jsi::Value(static_cast<ArkTSTurboModule&>(turboModule)
-                        .callAsync(rt, "getPromisedArray", args, count));
-}
-
 static jsi::Value
 __hostFunction_NativeSampleTurboCxxModuleSpecJSI_displayRNOHError(
     jsi::Runtime& rt,
@@ -121,24 +38,28 @@ NativeSampleTurboModuleSpecJSI::NativeSampleTurboModuleSpecJSI(
     const ArkTSTurboModule::Context ctx,
     const std::string name)
     : ArkTSTurboModule(ctx, name) {
-  methodMap_["voidFunc"] =
-      MethodMetadata{0, __hostFunction_NativeSampleTurboModuleSpecJSI_voidFunc};
-  methodMap_["getBool"] = MethodMetadata{
-      1, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getBool};
-  methodMap_["getNull"] = MethodMetadata{
-      1, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getNull};
-  methodMap_["getArray"] = MethodMetadata{
-      1, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getArray};
-  methodMap_["getString"] = MethodMetadata{
-      1, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getString};
-  methodMap_["getObject"] = MethodMetadata{
-      1, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getObject};
-  methodMap_["registerFunction"] = MethodMetadata{
-      1, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_registerFunction};
-  methodMap_["getPromisedArray"] = MethodMetadata{
-      0, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_getPromisedArray};
-  methodMap_["doAsyncJob"] = MethodMetadata{
-      0, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_doAsyncJob};
+    methodMap_ = {
+        ARK_METHOD_METADATA(voidFunc, 0),
+        ARK_METHOD_METADATA(getBool, 1),
+        ARK_METHOD_METADATA(getString, 1),
+        ARK_METHOD_METADATA(getObject, 1),
+        ARK_METHOD_METADATA(registerFunction, 1),
+        ARK_ASYNC_METHOD_METADATA(doAsyncJob, 1),
+        ARK_ASYNC_METHOD_METADATA(getPromisedArray, 0),
+        ARK_METHOD_METADATA(getUnionValue, 1),
+        ARK_METHOD_METADATA(getEnum, 3),
+        ARK_METHOD_METADATA(getUnknown, 1),
+        ARK_METHOD_METADATA(getRootTag, 1),
+        ARK_METHOD_METADATA(getNumber, 1),
+        ARK_METHOD_METADATA(getValue, 3),
+        ARK_METHOD_METADATA(getValueWithCallback, 1),
+        ARK_ASYNC_METHOD_METADATA(getValueWithPromise, 1),
+        ARK_METHOD_METADATA(getUnsafeObject, 1),
+        ARK_METHOD_METADATA(getConstants, 0),
+        ARK_METHOD_METADATA(getNull, 1),
+        ARK_METHOD_METADATA(getArray, 1),
+    };
+
   methodMap_["displayRNOHError"] = MethodMetadata{
       1, __hostFunction_NativeSampleTurboCxxModuleSpecJSI_displayRNOHError};
   methodMap_["throwExceptionCpp"] = MethodMetadata{
