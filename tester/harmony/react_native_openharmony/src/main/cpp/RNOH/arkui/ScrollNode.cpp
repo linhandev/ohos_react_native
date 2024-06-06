d9052cc6671a7000fc704db4a409f36c0c7fbfe8
Sebastian Szewczyk <sebastian.szewczyk@swmansion.com>
2024-06-06 13:27:50 +0200

    Changed default values for ScrollNode scrollTo animation
    ## Summary
    Changed default values for ScrollNode scrollTo animation
    
    ## Changes
    - changed default values for ScrollNode scrollTo animation
    
    ## Test Plan
    1. Open tester app.
    2. Open scrollview -> scroll to (animated) example, and see faster animation
    
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    See merge request rnoh/react-native-harmony!1256
    
    Signed-off-by: huangyouhua <1356795413@qq.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/arkui/ScrollNode.cpp
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/arkui/ScrollNode.cpp
@@ -140,8 +140,8 @@ void ScrollNode::scrollTo(float x, float y, bool animated) {
   ArkUI_NumberValue value[] = {
       {.f32 = x},
       {.f32 = y},
-      {.i32 = animated ? 1000 : 0},
-      {.i32 = ArkUI_AnimationCurve::ARKUI_CURVE_SMOOTH},
+      {.i32 = animated ? 250 : 0},
+      {.i32 = ArkUI_AnimationCurve::ARKUI_CURVE_LINEAR},
       {.i32 = 0}};
   item = {value, sizeof(value) / sizeof(ArkUI_NumberValue)};
   maybeThrow(NativeNodeApi::getInstance()->setAttribute(
