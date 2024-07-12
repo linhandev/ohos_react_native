linhandev/ohos_react_native

commit 642b4a0726eda5654b473bfdecb398a40ff74fa1

NativeAnimatedTM - Passing list of tags with props to update in single task
## Summary
NativeAnimatedTM - Passing list of tags with props to update in single task

## Changes
- optimized native animations by batching multiple updates in single task

## Test Plan
1. Open test case ([link](https://gl.swmansion.com/rnoh/react-native-harmony/-/issues/1127)).
2. Scroll left component
3. App remains usable

## Checklist
- [x] Project compiles and the tester application works.

## Issue Links
https://gl.swmansion.com/rnoh/react-native-harmony/-/issues/1127

See merge request rnoh/react-native-harmony!1346

Signed-off-by: huangyouhua <1356795413@qq.com>

tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/TurboModules/Animated/AnimatedNodesManager.cpp
@@ -24,10 +24,8 @@ using namespace facebook;
 namespace rnoh {
 
 AnimatedNodesManager::AnimatedNodesManager(
-    std::function<void()>&& scheduleUpdateFn,
-    std::function<void(react::Tag, folly::dynamic)>&& setNativePropsFn)
-    : m_scheduleUpdateFn(std::move(scheduleUpdateFn)),
-      m_setNativePropsFn(std::move(setNativePropsFn)) {}
+    std::function<void()>&& scheduleUpdateFn)
+    : m_scheduleUpdateFn(std::move(scheduleUpdateFn)) {}
 
 void AnimatedNodesManager::createNode(
     facebook::react::Tag tag,
@@ -245,7 +243,7 @@ void AnimatedNodesManager::stopAnimation(facebook::react::Tag animationId) {
   }
 }
 
-void AnimatedNodesManager::runUpdates(uint64_t frameTimeNanos) {
+PropUpdatesList AnimatedNodesManager::runUpdates(uint64_t frameTimeNanos) {
   // we don't want to enter this while updating nodes (which can happen if a
   // tracking node starts a new animation)
   m_isRunningAnimations = true;
@@ -259,7 +257,7 @@ void AnimatedNodesManager::runUpdates(uint64_t frameTimeNanos) {
     }
   }
 
-  updateNodes();
+  auto propUpdatesList = updateNodes();
 
   for (auto animationId : finishedAnimations) {
     m_animationById.at(animationId)->endCallback_(true);
@@ -273,13 +271,14 @@ void AnimatedNodesManager::runUpdates(uint64_t frameTimeNanos) {
     m_isRunningAnimations = true;
     m_scheduleUpdateFn();
   }
+  return propUpdatesList;
 }
 
 void AnimatedNodesManager::setNeedsUpdate(facebook::react::Tag nodeTag) {
   m_nodeTagsToUpdate.insert(nodeTag);
 }
 
-void AnimatedNodesManager::updateNodes() {
+PropUpdatesList AnimatedNodesManager::updateNodes() {
   std::vector<facebook::react::Tag> nodeTags(
       m_nodeTagsToUpdate.begin(), m_nodeTagsToUpdate.end());
   m_nodeTagsToUpdate.clear();
@@ -328,6 +327,8 @@ void AnimatedNodesManager::updateNodes() {
       nodeTagsQueue.push(node);
     }
   }
+
+  PropUpdatesList propUpdatesList;
   while (!nodeTagsQueue.empty()) {
     auto tag = nodeTagsQueue.front();
     nodeTagsQueue.pop();
@@ -338,7 +339,10 @@ void AnimatedNodesManager::updateNodes() {
 
       if (auto propsNode = dynamic_cast<PropsAnimatedNode*>(&node);
           propsNode != nullptr) {
-        propsNode->updateView();
+        auto propUpdate = propsNode->updateView();
+        if (propUpdate.has_value()) {
+          propUpdatesList.push_back(std::move(propUpdate.value()));
+        }
       }
 
       if (auto valueNode = dynamic_cast<ValueAnimatedNode*>(&node);
@@ -370,6 +374,8 @@ void AnimatedNodesManager::updateNodes() {
         " active nodes, but only " + std::to_string(updatedNodesCount) +
         " were updated");
   }
+
+  return propUpdatesList;
 }
 
 void AnimatedNodesManager::stopAnimationsForNode(facebook::react::Tag tag) {

tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/TurboModules/Animated/AnimatedNodesManager.h
@@ -11,6 +11,8 @@
 #include "Nodes/AnimatedNode.h"
 
 namespace rnoh {
+using PropUpdate = std::pair<facebook::react::Tag, folly::dynamic>;
+using PropUpdatesList = std::vector<PropUpdate>;
 
 class AnimatedNode;
 class ValueAnimatedNode;
@@ -19,10 +21,7 @@ class EventAnimationDriver;
 
 class AnimatedNodesManager {
  public:
-  AnimatedNodesManager(
-      std::function<void()>&& scheduleUpdateFn,
-      std::function<void(facebook::react::Tag, folly::dynamic)>&&
-          setNativePropsFn);
+  AnimatedNodesManager(std::function<void()>&& scheduleUpdateFn);
 
   void createNode(facebook::react::Tag tag, folly::dynamic const& config);
   void dropNode(facebook::react::Tag tag);
@@ -69,7 +68,7 @@ class AnimatedNodesManager {
       std::function<void(bool)>&& endCallback);
   void stopAnimation(facebook::react::Tag animationId);
 
-  void runUpdates(uint64_t frameTimeNanos);
+  PropUpdatesList runUpdates(uint64_t frameTimeNanos);
 
   void setNeedsUpdate(facebook::react::Tag nodeTag);
 
@@ -81,10 +80,8 @@ class AnimatedNodesManager {
   AnimatedNode& getNodeByTag(facebook::react::Tag tag);
   ValueAnimatedNode& getValueNodeByTag(facebook::react::Tag tag);
 
-  std::function<void(facebook::react::Tag, folly::dynamic)> m_setNativePropsFn;
-
  private:
-  void updateNodes();
+  PropUpdatesList updateNodes();
   void stopAnimationsForNode(facebook::react::Tag tag);
   void maybeStartAnimations();
 

tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/TurboModules/Animated/NativeAnimatedTurboModule.cpp
@@ -239,25 +239,14 @@ jsi::Value removeAnimatedEventFromView(
 NativeAnimatedTurboModule::NativeAnimatedTurboModule(
     const ArkTSTurboModule::Context ctx,
     const std::string name)
-    : rnoh::ArkTSTurboModule(ctx, name),
-      m_animatedNodesManager(
-          [this] {
-            m_vsyncListener->requestFrame(
-                [weakSelf = weak_from_this()](long long _timestamp) {
-                  if (auto self = weakSelf.lock()) {
-                    self->runUpdates();
-                  }
-                });
-          },
-          [this](auto tag, auto props) {
-            if (m_ctx.taskExecutor->isOnTaskThread(TaskThread::MAIN)) {
-              this->setNativeProps(tag, props);
-            } else {
-              m_ctx.taskExecutor->runTask(TaskThread::MAIN, [this, tag, props] {
-                this->setNativeProps(tag, props);
-              });
-            }
-          }) {
+    : rnoh::ArkTSTurboModule(ctx, name), m_animatedNodesManager([this] {
+        m_vsyncListener->requestFrame(
+            [weakSelf = weak_from_this()](long long _timestamp) {
+              if (auto self = weakSelf.lock()) {
+                self->runUpdates();
+              }
+            });
+      }) {
   methodMap_ = {
       {"startOperationBatch", {0, rnoh::startOperationBatch}},
       {"finishOperationBatch", {0, rnoh::finishOperationBatch}},
@@ -440,7 +429,23 @@ void NativeAnimatedTurboModule::runUpdates() {
     auto now = std::chrono::high_resolution_clock::now();
     auto frameTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
         now.time_since_epoch());
-    this->m_animatedNodesManager.runUpdates(frameTime.count());
+    auto tagsToUpdate =
+        this->m_animatedNodesManager.runUpdates(frameTime.count());
+
+    if (m_ctx.taskExecutor->isOnTaskThread(TaskThread::MAIN)) {
+      this->setNativeProps(tagsToUpdate);
+    } else {
+      m_ctx.taskExecutor->runTask(
+          TaskThread::MAIN,
+          [weakSelf = weak_from_this(),
+           tagsToUpdate = std::move(tagsToUpdate)] {
+            auto self = weakSelf.lock();
+            if (!self) {
+              return;
+            }
+            self->setNativeProps(tagsToUpdate);
+          });
+    }
   } catch (std::exception& e) {
     LOG(ERROR) << "Error in animation update: " << e.what();
     m_vsyncListener->requestFrame(
@@ -449,6 +454,16 @@ void NativeAnimatedTurboModule::runUpdates() {
             self->runUpdates();
           }
         });
+  }
+}
+
+void NativeAnimatedTurboModule::setNativeProps(
+    PropUpdatesList const& tagsToUpdate) {
+  if (auto instance = m_ctx.instance.lock(); instance != nullptr) {
+    for (auto const& [tag, props] : tagsToUpdate) {
+      instance->synchronouslyUpdateViewOnUIThread(tag, props);
+    }
+    return;
   }
 }
 

tester/harmony/react_native_openharmony/src/main/cpp/RNOH/MountingManagerCAPI.cpp
@@ -70,7 +70,10 @@ void MountingManagerCAPI::dispatchCommand(
     folly::dynamic const& args) {
   facebook::react::SystraceSection s(
       "RNOH::MountingManagerCAPI::dispatchCommand");
-  m_arkTSMountingManager->dispatchCommand(shadowView, commandName, args);
+
+  if (m_arkTSComponentNames.count(shadowView.componentName)) {
+    m_arkTSMountingManager->dispatchCommand(shadowView, commandName, args);
+  }
   auto componentInstance =
       m_componentInstanceRegistry->findByTag(shadowView.tag);
   if (componentInstance) {

tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/TurboModules/Animated/NativeAnimatedTurboModule.h
@@ -99,6 +99,7 @@ class NativeAnimatedTurboModule
   void runUpdates();
 
   void setNativeProps(facebook::react::Tag tag, folly::dynamic const& props);
+  void setNativeProps(PropUpdatesList const& propUpdatesList);
 
   void emitAnimationEndedEvent(
       facebook::jsi::Runtime& rt,

tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/TurboModules/Animated/Nodes/PropsAnimatedNode.h
@@ -33,11 +33,11 @@ class PropsAnimatedNode : public AnimatedNode {
     m_viewTag = std::nullopt;
   }
 
-  void updateView() {
+  std::optional<PropUpdate> updateView() {
     if (m_viewTag == std::nullopt) {
       LOG(WARNING)
           << "PropsAnimatedNode::updateView() called on unconnected node";
-      return;
+      return std::nullopt;
     }
     folly::dynamic props = folly::dynamic::object;
     for (auto& [key, nodeTag] : m_tagByPropName) {
@@ -52,8 +52,7 @@ class PropsAnimatedNode : public AnimatedNode {
         throw std::runtime_error("Unsupported property animated node type");
       }
     }
-
-    m_nodesManager.m_setNativePropsFn(*m_viewTag, props);
+    return std::make_pair(m_viewTag.value(), props);
   }
 
  private:

commit 1316e8c84ae59528e08b5dec200228eabbe694b5

fix: animated events don't update view properties
## Summary
642b4a0726eda5654b473bfdecb398a40ff74fa1 broke animated events. This PR fixes them.

## Changes
- fixed animated events not updating views when no other animations are running

## Test Plan
1. Open tester app.
2. Go to Animated test suite
3. Verify Animated.Event tests run properly


## Checklist
- [x] Project compiles and the tester application works.

See merge request rnoh/react-native-harmony!1354

Signed-off-by: huangyouhua <1356795413@qq.com>

tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/TurboModules/Animated/AnimatedNodesManager.cpp
@@ -152,7 +152,7 @@ void AnimatedNodesManager::stopListeningToAnimatedNodeValue(
   node.removeValueListener();
 }
 
-void AnimatedNodesManager::handleEvent(
+PropUpdatesList AnimatedNodesManager::handleEvent(
     facebook::react::Tag targetTag,
     std::string const& eventName,
     folly::dynamic const& eventValue) {
@@ -168,8 +168,9 @@ void AnimatedNodesManager::handleEvent(
   if (someDriverNeedsUpdate) {
     // NOTE: we don't update frame-based drivers here, only the newly-dirty ones
     // from the event
-    updateNodes();
+    return updateNodes();
   }
+  return {};
 }
 
 void AnimatedNodesManager::setValue(facebook::react::Tag tag, double value) {

tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/TurboModules/Animated/AnimatedNodesManager.h
@@ -72,7 +72,7 @@ class AnimatedNodesManager {
 
   void setNeedsUpdate(facebook::react::Tag nodeTag);
 
-  void handleEvent(
+  PropUpdatesList handleEvent(
       facebook::react::Tag targetTag,
       std::string const& eventName,
       folly::dynamic const& eventValue);

tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/TurboModules/Animated/NativeAnimatedTurboModule.cpp
@@ -451,6 +451,8 @@ void NativeAnimatedTurboModule::runUpdates() {
     m_vsyncListener->requestFrame(
         [weakSelf = weak_from_this()](long long _timestamp) {
           if (auto self = weakSelf.lock()) {
+            facebook::react::SystraceSection s(
+                "NativeAnimatedTurboModule::runUpdates");
             self->runUpdates();
           }
         });
@@ -518,8 +520,7 @@ void NativeAnimatedTurboModule::handleEvent(
   react::Tag tag = ctx.tag;
   auto eventName = ctx.eventName;
 
-  auto lock = acquireLock();
-  m_animatedNodesManager.handleEvent(tag, eventName, payload);
+  handleComponentEvent(tag, eventName, payload);
 }
 
 void NativeAnimatedTurboModule::initializeEventListener() {
@@ -536,6 +537,8 @@ void NativeAnimatedTurboModule::handleComponentEvent(
     std::string const& eventName,
     folly::dynamic payload) {
   auto lock = acquireLock();
-  m_animatedNodesManager.handleEvent(tag, eventName, payload);
+  auto propUpdates =
+      m_animatedNodesManager.handleEvent(tag, eventName, payload);
+  setNativeProps(propUpdates);
 }
 } // namespace rnoh