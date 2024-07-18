1606fe322c4ee9e6742cfca50deeb758ab26a2d0
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2024-07-18 14:06:49 +0200

    refactor: WorkerThread
    ## Summary
    Currently, RNInstanceRegistry is responsible for initializing Worker and it does it incorrectly because waits for worker readiness everytime RNInstance is created. This PR moves the responsibility to the WorkerThread class.
    
    ## Test Plan
    Check WorkerTurboModule tests and logs
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    ## Issue Links
    Closes https://gl.swmansion.com/rnoh/react-native-harmony/-/issues/1177
    
    See merge request rnoh/react-native-harmony!1377
    
    Signed-off-by: huangyouhua <1356795413@qq.com>

--- tester/harmony/react_native_openharmony/src/main/ets/RNOH/RNInstanceRegistry.ts
+++ tester/harmony/react_native_openharmony/src/main/ets/RNOH/RNInstanceRegistry.ts
@@ -10,7 +10,6 @@ import { HttpClient } from '../HttpClient/ts';
 import { DisplayMetricsManager } from "./DisplayMetricsManager"
 import resourceManager from '@ohos.resourceManager';
 import { WorkerThread } from "./WorkerThread"
-import { RNOHError } from "./RNOHError"
 
 export class RNInstanceRegistry {
   private instanceMap: Map<number, RNInstanceImpl> = new Map();
@@ -26,30 +25,22 @@ export class RNInstanceRegistry {
     private defaultHttpClient: HttpClient | undefined, // TODO: remove "undefined" when HttpClientProvider is removed
     private resourceManager: resourceManager.ResourceManager,
     private displayMetricsManager: DisplayMetricsManager,
-    private workerThread: WorkerThread | null,
+    private workerThreadPromise: Promise<WorkerThread> | null,
   ) {
-    if (this.workerThread) {
-      this.unregisterMessageListeners.push(this.workerThread.subscribeToMessages((type, payload) => {
-        if (type === "RNOH_ERROR") {
-          const rnohError = new RNOHError({ whatHappened: payload.message, howCanItBeFixed: [], customStack: payload.stack});
-          this.devToolsController.setLastError(rnohError)
-          this.devToolsController.eventEmitter.emit("NEW_ERROR", rnohError)
-        }
-      }))
-    }
   }
 
   public async createInstance(options: RNInstanceOptions): Promise<RNInstance> {
     const id = this.napiBridge.getNextRNInstanceId();
-    if (this.workerThread !== null) {
-      await this.waitForWorkerThread(id, options?.name)
+    let workerThread: WorkerThread | null = null
+    if (this.workerThreadPromise !== null) {
+      workerThread = await this.workerThreadPromise
+      await this.createRNInstanceEnvOnWorker(workerThread, id, options?.name)
     }
     const fontFamilyNameByFontPathRelativeToRawfileDir: Record<string, string> = {}
     for (const [fontFamily, fontResource] of Object.entries(options.fontResourceByFontFamily ?? {})) {
       fontFamilyNameByFontPathRelativeToRawfileDir[fontFamily] = fontResource.params[0]
       font.registerFont({ familyName: fontFamily, familySrc: fontResource })
     }
-
     const rnInstance = new RNInstanceImpl(
       this.envId,
       id,
@@ -58,7 +49,7 @@ export class RNInstanceRegistry {
       { concurrentRoot: options.disableConcurrentRoot !== undefined ? !options.disableConcurrentRoot : true },
       this.devToolsController,
       this.createRNOHContext,
-      this.workerThread,
+      workerThread,
       options.enableDebugger ?? false,
       options.enableBackgroundExecutor ?? false,
       options.enableNDKTextMeasuring ?? false,
@@ -79,38 +70,15 @@ export class RNInstanceRegistry {
     return rnInstance;
   }
 
-  private async waitForWorkerThread(rnInstanceId: number, rnInstanceName: string) {
-    const logger = this.logger.clone(["RNInstanceRegistry", "waitForWorkerThread"])
-    let resolveWorkerTurboModuleProviderRegisteredPromise = (value: undefined) => undefined
-    const workerTurboModuleProviderRegisteredPromise = new Promise((resolve) => {
-      resolveWorkerTurboModuleProviderRegisteredPromise = resolve
-    })
-
-    const postMessage = (type: string, payload: any = undefined) => {
-      logger.info(`postMessage: ${type}`)
-      this.workerThread.postMessage(type, payload)
-    }
-
-    logger.info("waiting for worker readiness")
-    await new Promise((resolve) => {
-      const readinessProb = setInterval(() => postMessage("RNOH_WORKER_THREAD_READY"), 100);
-      this.unregisterMessageListeners.push(this.workerThread.subscribeToMessages((type, payload) => {
-        logger.info(`receivedMessage: ${type}`)
-        if (type === "RNOH_WORKER_THREAD_READY_ACK") {
-          clearInterval(readinessProb);
-          resolve(undefined)
-        } else if (type === "RNOH_CREATE_RN_INSTANCE_WORKER_ENV_ACK") {
-          resolveWorkerTurboModuleProviderRegisteredPromise(undefined)
-        }
-      }))
-
-    })
-
-    postMessage("RNOH_CREATE_RN_INSTANCE_WORKER_ENV", {
-      rnInstanceId, rnInstanceName
-    })
-    logger.info("waiting for workerTurboModuleProvider")
-    await workerTurboModuleProviderRegisteredPromise;
+  private async createRNInstanceEnvOnWorker(workerThread: WorkerThread, rnInstanceId: number, rnInstanceName: string) {
+    const logger = this.logger.clone(["RNInstanceRegistry", "createRNInstanceEnvOnWorker"])
+    logger.info("waiting for worker's rnInstance environment")
+    setTimeout(() => {
+      workerThread.postMessage("RNOH_CREATE_RN_INSTANCE_WORKER_ENV", {
+        rnInstanceId, rnInstanceName
+      })
+    }, 0)
+    await workerThread.waitForMessage("RNOH_CREATE_RN_INSTANCE_WORKER_ENV_ACK")
   }
 
   public getInstance(id: number): RNInstance {
--- tester/harmony/react_native_openharmony/src/main/ets/RNOH/WorkerThread.ts
+++ tester/harmony/react_native_openharmony/src/main/ets/RNOH/WorkerThread.ts
@@ -1,33 +1,74 @@
 import worker from "@ohos.worker"
+import { RNOHLogger } from "./RNOHLogger"
+import { RNOHError } from "./RNOHError"
 
 interface WorkerThreadDependencies {
   scriptUrl: string
   name: string
+  logger: RNOHLogger
+  onWorkerError: (rnohError: RNOHError) => void
 }
 
 type WorkerThreadMessageListener = (string, unknown) => void
 
+/**
+ * @internal
+ */
 export class WorkerThread {
-  static create({ scriptUrl, name }: WorkerThreadDependencies) {
-    return new WorkerThread(new worker.ThreadWorker(scriptUrl, { name: name }))
+  static async create({ scriptUrl, name, logger, onWorkerError }: WorkerThreadDependencies) {
+    const workerThread = new WorkerThread(logger, new worker.ThreadWorker(scriptUrl, { name: name }), onWorkerError)
+    const interval = setInterval(() => {
+      workerThread.postMessage("RNOH_WORKER_THREAD_READY")
+    }, 100);
+    setTimeout(() => {
+      workerThread.postMessage("RNOH_WORKER_THREAD_READY")
+    }, 0)
+    await workerThread.waitForMessage("RNOH_WORKER_THREAD_READY_ACK")
+    clearInterval(interval)
+    return workerThread;
   }
 
+  private logger: RNOHLogger
   private listeners: Set<WorkerThreadMessageListener> = new Set()
 
-  private constructor(private rawWorkerThread: worker.ThreadWorker) {
+  private constructor(logger: RNOHLogger, private rawWorkerThread: worker.ThreadWorker,
+    private onWorkerError: (rnohError: RNOHError) => void) {
+    this.logger = logger.clone("WorkerThread")
     this.rawWorkerThread.onmessage = (ev) => {
       const messageType = ev.data?.type
       const messagePayload = ev.data?.payload
       if (messageType) {
+        this.logger.clone("receivedMessage").debug(messageType)
         for (const listener of this.listeners) {
           listener(messageType, messagePayload)
         }
       }
     }
+    this.listeners.add((type, payload) => {
+      if (type === "RNOH_ERROR") {
+        this.onWorkerError(new RNOHError({
+          whatHappened: payload.message,
+          howCanItBeFixed: [],
+          customStack: payload.stack
+        }))
+      }
+    })
   }
 
   postMessage(type: string, payload: any = undefined) {
-    this.rawWorkerThread.postMessage({type, payload})
+    this.logger.clone("postMessage").debug(type)
+    this.rawWorkerThread.postMessage({ type, payload })
+  }
+
+  async waitForMessage(expectedMsgType: string) {
+    return new Promise(resolve => {
+      const unsubscribe = this.subscribeToMessages((type) => {
+        if (type === expectedMsgType) {
+          resolve(undefined)
+          unsubscribe()
+        }
+      })
+    })
   }
 
   subscribeToMessages(listener: WorkerThreadMessageListener): () => void {
