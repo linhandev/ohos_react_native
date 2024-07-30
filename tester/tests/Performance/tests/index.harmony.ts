21b4195bca774ccea90b5ffcfb42c3218de37391
Patryk Klatka <patryk.klatka@swmansion.com>
2024-07-30 15:05:30 +0200

    Fix "react-native-gesture-handler" error when running tester on Android
    ## Summary
    This PR fixes the occurring "react-native-gesture-handler" error when running tester on Android - from now, HarmonyOS-specific tests can be added to `index.harmony.ts`
    
    ## Test Plan
    1. Run the tester app on Android and see if "react-native-gesture-handler" error occurs.
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    ## Issue Links
    Resolves: #1201
    
    See merge request rnoh/react-native-harmony!1438
    
    Signed-off-by: huangyouhua <1356795413@qq.com>

--- tester/tests/Performance/tests/index.harmony.ts
+++ tester/tests/Performance/tests/index.harmony.ts
@@ -2,6 +2,11 @@ export * from './SquareRotationAnimationTestWithNativeDriver';
 export * from './SquareRotationAnimationTestWithoutNativeDriver';
 export * from './Render1500ColorfulViews';
 export * from './Render1500Views';
+// StackNavigator tests will fail on Android because of the missing react-native-gesture-handler dependency
+export * from './StackNavigatorGoBackAction';
+export * from './StackNavigatorGoBackActionToComplexScreen';
+export * from './StackNavigatorNavigateAction';
+export * from './StackNavigatorNavigateActionToComplexScreen';
 export * from './RenderFlatListWith250Views';
 export * from './RenderScrollViewWith500Views';
 export * from './RenderFlatListWithAnimatedViews';
