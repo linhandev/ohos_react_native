4b83cea757cf8f78d328f1f08045c9e745c166a9
quxin024 <quixn9@huawei.com>
2024-08-06 21:20:23 +0800

    Revert "allow getting native node id by tag on ArkTS side"
    This reverts commit b216871e997ad808d75326644bf11ae94c7c1cbc.

--- tester/harmony/react_native_openharmony/src/main/ets/RNOH/RNInstance.ts
+++ tester/harmony/react_native_openharmony/src/main/ets/RNOH/RNInstance.ts
@@ -242,12 +242,6 @@ export interface RNInstance {
    * register an appropriate onScroll callback and call this method.
    */
   cancelTouches(): void
-
-  /**
-   * @architecture: C-API
-   * Retrieves the native ArkUI node's `id` attribute for the React component with given tag.
-   */
-  getNativeNodeIdByTag(tag: Tag): string | undefined
 }
 
 export type RNInstanceOptions = {
