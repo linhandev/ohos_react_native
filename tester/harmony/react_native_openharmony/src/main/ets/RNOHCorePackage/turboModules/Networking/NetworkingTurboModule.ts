abc49a08f545af032de5c58363ce2334421abe5b
cwx1172642 <chengyuanhang3@h-partners.com>
2024-08-09 16:03:55 +0800

    [TicketNo:] NA [Description:] 修复axios发送请求中文需要encodeURI预处理的问题 [Binary Source:]NA

--- tester/harmony/react_native_openharmony/src/main/ets/RNOHCorePackage/turboModules/Networking/NetworkingTurboModule.ts
+++ tester/harmony/react_native_openharmony/src/main/ets/RNOHCorePackage/turboModules/Networking/NetworkingTurboModule.ts
@@ -179,6 +179,23 @@ export class NetworkingTurboModule extends TurboModule {
     callback(didDeleteAnyCookies);
   }
 
+  private isEncodedURI(str: string): boolean {
+    try {
+      const decodedStr = decodeURI(str);
+      return decodedStr !== str;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  private getEncodedURI(str: string): string {
+    if (this.isEncodedURI(str)) {
+      return str;
+    }
+    return encodeURI(str);
+  }
+  
+
   async sendRequest(query: Query, callback: (requestId: number) => void) {
     const httpClient = this.ctx.rnInstance.httpClient;
     const requestId = this.createId();
@@ -241,7 +258,7 @@ export class NetworkingTurboModule extends TurboModule {
     };
 
 
-    const { cancel, promise } = httpClient.sendRequest(query.url,
+    const { cancel, promise } = httpClient.sendRequest(this.getEncodedURI(query.url),
       {
         method: this.REQUEST_METHOD_BY_NAME[query.method],
         header: query.headers,
