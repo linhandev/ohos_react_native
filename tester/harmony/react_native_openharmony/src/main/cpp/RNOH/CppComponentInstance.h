2e5f102183b1d1d76941b93114cb89f4b40f91ef
chenhaoyue <33661883+haoyuedada@users.noreply.github.com>
2024-08-09 20:08:24 +0800

    Revert "fix: An issue that cannot be clicked after using the
    Signed-off-by: hyh <huangyouhua2@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/CppComponentInstance.h
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/CppComponentInstance.h
@@ -190,7 +190,10 @@ class CppComponentInstance : public ComponentInstance {
   }
 
   facebook::react::Transform getTransform() const override {
-    return m_transform;    
+    if (m_props != nullptr) {
+      return m_props->transform;
+    }
+    return facebook::react::Transform::Identity();
   }
 
   facebook::react::Rect getBoundingBox() override {
@@ -321,7 +324,6 @@ class CppComponentInstance : public ComponentInstance {
          abs(m_oldPointScaleFactor - m_layoutMetrics.pointScaleFactor) >
              0.001f) {
         m_oldPointScaleFactor = m_layoutMetrics.pointScaleFactor;
-        this->setTransform(props->transform);
         this->getLocalRootArkUINode().setTransform(
           props->transform, m_layoutMetrics.pointScaleFactor);
         markBoundingBoxAsDirty();
@@ -448,13 +450,6 @@ class CppComponentInstance : public ComponentInstance {
   };
 
  private:
-  facebook::react::Transform m_transform =
-      facebook::react::Transform::Identity();  
-    
-  void setTransform(facebook::react::Transform const& transform) {
-    m_transform = transform;
-  }
-    
   void setOpacity(facebook::react::SharedViewProps const& props) {
     auto isOpacityManagedByAnimated = getIgnoredPropKeys().count("opacity") > 0;
     auto isTransformManagedByAnimated =
