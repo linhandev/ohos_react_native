93244c16533bda1ebf2151e0f9f9753b8f058ced
quxin024 <60676727+quxin024@users.noreply.github.com>
2024-08-21 22:36:29 +0800

    Fixed removeClippedSubviews after changing to horizontal (#795)
    Co-authored-by: quxin024 <quixn9@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/ComponentInstances/ScrollViewComponentInstance.cpp
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/ComponentInstances/ScrollViewComponentInstance.cpp
@@ -51,12 +51,7 @@ void ScrollViewComponentInstance::setLayout(
 }
 
 void rnoh::ScrollViewComponentInstance::updateOffsetAfterChildChange(
-    facebook::react::Point offset,
-    double diff) {
-  if (diff <= 0) {
-    return;
-  }
-
+    facebook::react::Point offset) {
   if (isHorizontal(m_props)) {
     if (offset.x + m_containerSize.width <= m_contentSize.width) {
       return;
@@ -91,12 +86,8 @@ void rnoh::ScrollViewComponentInstance::onStateChanged(
   CppComponentInstance::onStateChanged(state);
   auto stateData = state->getData();
   if (m_contentSize != stateData.getContentSize()) {
-    double diff = isHorizontal(m_props)
-        ? m_contentSize.width - stateData.getContentSize().width
-        : m_contentSize.height - stateData.getContentSize().height;
     m_contentContainerNode.setSize(stateData.getContentSize());
     m_contentSize = stateData.getContentSize();
-    updateOffsetAfterChildChange(getCurrentOffset(), diff);
   }
 }
 
@@ -543,6 +534,7 @@ void ScrollViewComponentInstance::onFinalizeUpdates() {
     }
     m_shouldAdjustScrollPositionOnNextRender = false;
   }
+  updateOffsetAfterChildChange(getCurrentOffset());
 }
 
 folly::dynamic ScrollViewComponentInstance::getScrollEventPayload(
