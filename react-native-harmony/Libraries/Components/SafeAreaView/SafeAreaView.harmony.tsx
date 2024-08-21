8a1170e9e80f81647243143fb5a40b544bbc3944
chenhaoyue <33661883+haoyuedada@users.noreply.github.com>
2024-08-21 20:21:09 +0800

    fix: nested SafeAreaViews + change: make SafeAreaView apply
    Signed-off-by: hyh <huangyouhua2@huawei.com>

--- react-native-harmony/Libraries/Components/SafeAreaView/SafeAreaView.harmony.tsx
+++ react-native-harmony/Libraries/Components/SafeAreaView/SafeAreaView.harmony.tsx
@@ -23,31 +23,40 @@ const safeAreaTurboModule = TurboModuleRegistry.get<Spec>(
 )!;
 
 const getPaddingTop = (inset: number, pageY: number) => {
-  return Math.max(0, inset - pageY);
+  return Math.max(0, inset - (pageY < 0 ? pageY * -1 : pageY));
 }
 
 const getPaddingBottom = (insetBottom: number, insetTop: number, paddingTop: number, height: number, windowHeight: number, pageY: number, positionY: number): number => {
   // if SafeArea is not visible or outside the viewport or topped and not full height
-  if (height === 0 || pageY >= windowHeight || (pageY === 0 && height < windowHeight)) {
-    return 0;
+  if (height === 0 || (pageY === 0 && height < windowHeight)) {
+    return Math.max(0, insetBottom - (Math.round(windowHeight) - Math.round(height)));
   }
 
+  // if SafeAreaView is not topped and not full height and not visible
+  if (pageY < windowHeight && pageY > height && positionY === 0) {
+    return 0;
+  };
+
+  
   // if SafeAreaView is topped, check for full height
   if (Math.round(height) >= Math.round(windowHeight) && pageY === 0) {
     // if SafeAreaView is full height and at the top without any offset
     return positionY === 0 ? insetBottom : 0;
   }
-
+  
   // if SafeAreaView is topped with margin and not full height
   if (height < windowHeight && positionY === 0 && pageY <= insetTop) {
     return Math.max(0, insetBottom - (windowHeight - (height + pageY)));
   }
 
-  // if SafeAreaView is not topped and not full height and is nested
-  if (height < windowHeight && pageY > 0) {
+  if (height < windowHeight && pageY < windowHeight && pageY > 0 && positionY > 0) {
     return Math.max(0, insetBottom - (windowHeight - pageY));
   }
 
+  // if SafeAreaView is not topped and not full height and is nested
+  if (height < windowHeight && pageY > 0 && pageY > windowHeight) {
+    return Math.max(0, insetBottom);
+  }
   
   // Default case handling scenarios not captured above
   return Math.max(0, insetBottom - (windowHeight - height + paddingTop));
@@ -55,6 +64,7 @@ const getPaddingBottom = (insetBottom: number, insetTop: number, paddingTop: num
 
 export default React.forwardRef<View, ViewProps>(
   ({ children, style, ...otherProps }, ref) => {
+
     const safeAreaViewRef = React.useRef<View>(null);
 
     const [topInset, setTopInset] = useState(safeAreaTurboModule.getInitialInsets().top);
@@ -62,7 +72,7 @@ export default React.forwardRef<View, ViewProps>(
     const [rightInset, setRightInset] = useState(safeAreaTurboModule.getInitialInsets().right);
     const [bottomInset, setBottomInset] = useState(safeAreaTurboModule.getInitialInsets().bottom);
 
-    const [measurement, setMeasurement] = useState({ x: 0, y: 0, width: 0, height: 0, pageX: 0, pageY: 0 });
+    const [measurement, setMeasurement] = useState({ x: 0, y: 0, width: 0, height: 0, pageX: 0, pageY: -1 });
     const [layout, setLayout] = useState({ x: 0, y: 0, width: 0, height: 0 });
 
     const measureView = () => {
@@ -73,6 +83,7 @@ export default React.forwardRef<View, ViewProps>(
   
     useEffect(
       function subscribeToSafeAreaChanges() {
+
         const subscription = (RCTDeviceEventEmitter as any).addListener(
           "SAFE_AREA_INSETS_CHANGE",
           (insets: SafeAreaInsets) => {
@@ -86,28 +97,32 @@ export default React.forwardRef<View, ViewProps>(
           subscription.remove();
         };
       },
-      [setTopInset, setLeftInset, setRightInset, setBottomInset]
+      [setTopInset, setLeftInset, setRightInset, setBottomInset, measurement.pageY]
     );
 
     useEffect(() => {
       measureView();
     }, []);
 
-    
+    const isPaddingBottomExplicit = style?.paddingBottom !== undefined;
+    const isPaddingTopExplicit = style?.paddingTop !== undefined;
+    const isPaddingLeftExplicit = style?.paddingLeft !== undefined;
+    const isPaddingRightExplicit = style?.paddingRight !== undefined;
+
     const windowHeight = Dimensions.get('window').height;
     const paddingTop = getPaddingTop(topInset, measurement.pageY);
     const paddingBottom = getPaddingBottom(bottomInset, topInset, paddingTop, measurement.height, windowHeight, measurement.pageY, layout.y);
-    
+
     return (
       <View
         ref={safeAreaViewRef}
         style={[
           style,
           {
-            paddingTop: paddingTop,
-            paddingLeft: leftInset,
-            paddingRight: rightInset,
-            paddingBottom: paddingBottom,
+            paddingTop: isPaddingTopExplicit ? style.paddingBottom : paddingTop,
+            paddingLeft: isPaddingLeftExplicit ? style.paddingLeft : leftInset,
+            paddingRight: isPaddingRightExplicit ? style.paddingRight : rightInset,
+            paddingBottom: isPaddingBottomExplicit ? style.paddingBottom : paddingBottom,
           },
         ]}
         onLayout={(event) => {
