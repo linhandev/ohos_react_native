f73ced99234b9d234ef9e2738ce8e8d3367438ef
huangyouhua <1356795413@qq.com>
2024-08-23 12:38:11 +0800

    feat: 增加jsvm适配代码
    Signed-off-by: hyh <huangyouhua2@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/RNInstanceArkTS.cpp
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/RNInstanceArkTS.cpp
@@ -14,6 +14,7 @@
 #include "RNOH/TurboModuleFactory.h"
 #include "RNOH/TurboModuleProvider.h"
 #include "hermes/executor/HermesExecutorFactory.h"
+#include "JSVMExecutorFactory.h"
 #include "RNOH/SchedulerDelegate.h"
 
 using namespace facebook;
@@ -42,17 +43,16 @@ void RNInstanceArkTS::initialize() {
   m_eventDispatcher = std::make_shared<EventDispatcher>();
   std::vector<std::unique_ptr<react::NativeModule>> modules;
   auto instanceCallback = std::make_unique<react::InstanceCallback>();
-  auto jsExecutorFactory = std::make_shared<react::HermesExecutorFactory>(
-      // runtime installer, which is run when the runtime
-      // is first initialized and provides access to the runtime
-      // before the JS code is executed
-      [](facebook::jsi::Runtime& rt) {
+auto jsExecutorFactory = std::make_shared<rnjsvm::JSVMExecutorFactory>(
+        // runtime installer, which is run when the runtime
+        // is first initialized and provides access to the runtime
+        // before the JS code is executed
+        [](facebook::jsi::Runtime& rt) {
         // install `console.log` (etc.) implementation
         react::bindNativeLogger(rt, nativeLogger);
         // install tracing functions
         rnoh::setupTracing(rt);
-      });
-  jsExecutorFactory->setEnableDebugger(m_shouldEnableDebugger);
+        });
   m_jsQueue = std::make_shared<MessageQueueThread>(this->taskExecutor);
   auto moduleRegistry =
       std::make_shared<react::ModuleRegistry>(std::move(modules));
