83e416424b4dc81ba19a82ae75c0c46b43e3c4d1
w60035026 <yue5063657@163.com>
2024-09-13 10:02:13 +0800

    fix: fix multiInstance crash by thread change
    Signed-off-by: hyh <huangyouhua2@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/RNInstanceCAPI.h
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/RNInstanceCAPI.h
@@ -91,7 +91,12 @@ class RNInstanceCAPI : public RNInstanceInternal,
         m_componentInstanceFactory(componentInstanceFactory),
         m_arkTSChannel(std::move(arkTSChannel)),
         m_arkTSMessageHandlers(std::move(arkTSMessageHandlers)),
-        m_nativeResourceManager(std::move(nativeResourceManager)) {}
+        m_nativeResourceManager(std::move(nativeResourceManager)) {
+          this->unsubscribeUITickListener =
+              this->m_uiTicker->subscribe(m_id, [this](long long timestamp){ 
+              this->taskExecutor->runTask(
+                TaskThread::MAIN, [this, timestamp](){ this->onUITick(timestamp); }); });
+        }
 
  ~RNInstanceCAPI() noexcept override;
 
