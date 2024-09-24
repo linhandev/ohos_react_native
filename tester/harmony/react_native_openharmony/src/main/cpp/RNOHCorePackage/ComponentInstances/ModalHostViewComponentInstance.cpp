bb04b38c47ccc524568e3463e6cf80ffccd833ec
17889967223 <55738645+17889967223@users.noreply.github.com>
2024-09-24 17:16:27 +0800

    回退modal被删除的position设置 (#888)
    Co-authored-by: l30042487 <lixingda1@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/ComponentInstances/ModalHostViewComponentInstance.cpp
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOHCorePackage/ComponentInstances/ModalHostViewComponentInstance.cpp
@@ -73,6 +73,20 @@ void ModalHostViewComponentInstance::updateDisplaySize(
   }
 }
 
+void ModalHostViewComponentInstance::resetModalPosition(
+    DisplayMetrics const& displayMetrics,
+    SharedConcreteState const& state) {
+  auto windowMetrics = displayMetrics.windowPhysicalPixels;
+  FoldStatus foldStatus  = static_cast<FoldStatus>(ArkTSBridge::getInstance()->getFoldStatus());
+  auto isSplitScreenMode = ArkTSBridge::getInstance()->getIsSplitScreenMode();
+  if ((foldStatus == FOLD_STATUS_EXPANDED || foldStatus == FOLD_STATUS_HALF_FOLDED) && isSplitScreenMode) {
+    m_rootStackNode.setPosition(
+        {displayMetrics.screenPhysicalPixels.width / windowMetrics.scale, 0});
+    } else {
+        m_rootStackNode.setPosition({0, 0});
+    }
+}
+
 void ModalHostViewComponentInstance::updateSlideTransition(
     DisplayMetrics const& displayMetrics) {
   auto screenSize = displayMetrics.windowPhysicalPixels;
@@ -89,6 +103,11 @@ ModalHostViewComponentInstance::ModalHostViewComponentInstance(Context context)
     FoldStatus foldStatus  = static_cast<FoldStatus>(ArkTSBridge::getInstance()->getFoldStatus());
     auto isSplitScreenMode = ArkTSBridge::getInstance()->getIsSplitScreenMode();
     auto displayMetrics = ArkTSBridge::getInstance()->getDisplayMetrics();
+    if (foldStatus == FOLD_STATUS_EXPANDED && isSplitScreenMode) {
+        m_rootStackNode.setPosition({displayMetrics.screenPhysicalPixels.width / displayMetrics.windowPhysicalPixels.scale, 0});
+    } else {
+        m_rootStackNode.setPosition({0, 0});
+    }
 }
 
 void ModalHostViewComponentInstance::setLayout(facebook::react::LayoutMetrics layoutMetrics) {
@@ -182,6 +201,7 @@ void ModalHostViewComponentInstance::onMessageReceived(
   if (message.name == "WINDOW_SIZE_CHANGE") {
     auto displayMetrics = ArkTSBridge::getInstance()->getDisplayMetrics();
     updateDisplaySize(displayMetrics, m_state);
+    resetModalPosition(displayMetrics, m_state);
   }
 }
 
