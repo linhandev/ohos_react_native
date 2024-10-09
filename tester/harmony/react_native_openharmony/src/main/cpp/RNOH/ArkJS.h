7fea3610a9a759f71e1bfe0791ab7bcdf5389cae
w60035026 <yue5063657@163.com>
2024-10-09 15:48:34 +0800

    fix:move createCodeCache to jsQueue
    Signed-off-by: hyh <huangyouhua2@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/ArkJS.h
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/ArkJS.h
@@ -19,7 +19,6 @@
 
 class RNOHNapiObjectBuilder;
 class RNOHNapiObject;
-class NapiRef;
 
 class ArkJS {
  public:
@@ -59,8 +58,6 @@ class ArkJS {
 
   void deleteReference(napi_ref reference);
 
-  NapiRef createNapiRef(napi_value value);
-
   napi_value createSingleUseCallback(
       std::function<void(std::vector<folly::dynamic>)>&& callback);
 
@@ -98,8 +95,6 @@ class ArkJS {
 
   napi_value getReferenceValue(napi_ref ref);
 
-  napi_value getReferenceValue(NapiRef const& ref);
-
   std::vector<napi_value> getCallbackArgs(napi_callback_info info);
 
   std::vector<napi_value> getCallbackArgs(
@@ -109,8 +104,6 @@ class ArkJS {
   RNOHNapiObject getObject(napi_value object);
 
   RNOHNapiObject getObject(napi_ref objectRef);
-
-  RNOHNapiObject getObject(NapiRef const& objectRef);
 
   napi_value getObjectProperty(napi_value object, std::string const& key);
 
@@ -240,43 +233,6 @@ class Promise {
  private:
   ArkJS m_arkJS;
   napi_value m_value;
-};
-
-class NapiRef final {
- public:
-  NapiRef() = default;
-
-  // rule of five constructors
-  NapiRef(NapiRef const&) = default;
-  NapiRef& operator=(NapiRef const&) = default;
-
-  NapiRef(NapiRef&&) = default;
-  NapiRef& operator=(NapiRef&&) = default;
-
-  ~NapiRef() = default;
-
-  operator bool() const {
-    return m_ref != nullptr;
-  }
-
- private:
-  class Deleter {
-   public:
-    void operator()(napi_ref ref) const {
-      m_threadGuard.assertThread();
-      napi_delete_reference(m_env, ref);
-    }
-
-    napi_env m_env;
-    rnoh::ThreadGuard m_threadGuard;
-  };
-
-  NapiRef(napi_env env, napi_ref ref) : m_env(env), m_ref(ref, Deleter{env}) {}
-
-  napi_env m_env{};
-  std::shared_ptr<napi_ref__> m_ref{nullptr};
-
-  friend class ArkJS;
 };
 
 #endif // native_ArkJS_H
