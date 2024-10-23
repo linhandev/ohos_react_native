da35ecbb952f3c680a373eba1c76b644c22b0062
quxin024 <quxin9@huawei.com>
2024-10-23 15:09:36 +0800

    remove taskExecutor from EventBeat
    Signed-off-by: 瞿欣 <quxin9@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/RNInstanceArkTS.cpp
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/RNInstanceArkTS.cpp
@@ -69,10 +69,8 @@ void RNInstanceArkTS::initializeScheduler(
   m_contextContainer->insert("ReactNativeConfig", std::move(reactConfig));
 
   react::EventBeat::Factory eventBeatFactory =
-      [taskExecutor = std::weak_ptr(taskExecutor),
-       runtimeExecutor = this->instance->getRuntimeExecutor()](auto ownerBox) {
-        return std::make_unique<EventBeat>(
-            taskExecutor, runtimeExecutor, ownerBox);
+      [runtimeExecutor = this->instance->getRuntimeExecutor()](auto ownerBox) {
+        return std::make_unique<EventBeat>(runtimeExecutor, ownerBox);
       };
 
   react::ComponentRegistryFactory componentRegistryFactory =
