a26036ba21240eb9c86af2eccf9881b8983d4444
bulloo <63528916+bulloo@users.noreply.github.com>
2024-10-23 18:11:07 +0800

    fix: deadlock when calling TurboModuleProvider::getTurboModule (#987)
    ## Changes
    - fixed possible deadlock when calling TurboModuleProvider::getTurboModule
    
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    ## Issue Links
    Closes #1335
    
    See merge request rnoh/react-native-harmony!1649
    
    Co-authored-by: Juliusz Wajgelt <juliusz.wajgelt@swmansion.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/TurboModuleProvider.cpp
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/TurboModuleProvider.cpp
@@ -57,15 +57,18 @@ void TurboModuleProvider::installJSBindings(
 
 std::shared_ptr<react::TurboModule> TurboModuleProvider::getTurboModule(
     std::string const& moduleName) {
-  std::unique_lock lock(m_cacheMtx);
-  if (auto it = m_cache.find(moduleName); it != m_cache.end()) {
-    DLOG(INFO) << "Cache hit. Providing '" << moduleName << "' Turbo Module";
-    return it->second;
+  {
+    std::unique_lock lock(m_cacheMtx);
+    if (auto it = m_cache.find(moduleName); it != m_cache.end()) {
+      DLOG(INFO) << "Cache hit. Providing '" << moduleName << "' Turbo Module";
+      return it->second;
+    }
   }
   auto turboModule = m_createTurboModule(moduleName, m_jsInvoker, m_scheduler);
   if (turboModule != nullptr) {
-    m_cache.emplace(moduleName, turboModule);
-    return turboModule;
+    std::unique_lock lock(m_cacheMtx);
+    auto [it, inserted] = m_cache.emplace(moduleName, std::move(turboModule));
+    return it->second;
   }
   LOG(ERROR) << "Couldn't provide turbo module \"" << moduleName << "\"";
   return nullptr;
