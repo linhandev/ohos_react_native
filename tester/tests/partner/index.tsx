2fca74733a3b594ba7e55b15215ccdace0156feb
alvancf <742997447@qq.com>
2025-01-10 17:11:44 +0800

    实现deprecated-react-native-prop-types文件在rn引入
    Signed-off-by: alvancf <742997447@qq.com>

--- tester/tests/partner/index.tsx
+++ tester/tests/partner/index.tsx
@@ -1,19 +1,15 @@
 // Test cases for some reference situations
 import React, {useState, useEffect} from 'react';
-import {View, Text, Button, DeviceEventEmitter} from 'react-native';
+import {View, Text, Button, DeviceEventEmitter, ViewPropTypes} from 'react-native';
+// test DeprecatedStyleSheetPropType
 import StylesheetPropType from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedStyleSheetPropType';
 console.log('StylesheetPropType666: ', StylesheetPropType);
+// test DeprecatedViewPropTypes
 import ViewStylePropTypes from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedViewPropTypes';
 console.log('ViewStylePropTypes7777: ', ViewStylePropTypes);
-import {
-    TouchableOpacity,
-    Text,
-    StyleSheet,
-    View,
-    ,
-  } from 'react-native';
-  // import {ViewPropTypes} from 'deprecated-react-native-prop-types';
-  console.log('ViewPropTypes: ', ViewPropTypes);
+// test ViewPropTypes in react-native,not in deprecated-react-native-prop-types
+// import {ViewPropTypes} from 'deprecated-react-native-prop-types';
+console.log('ViewPropTypes: ', ViewPropTypes);
 
 export function LeadInTest() {
 
