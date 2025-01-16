461f248d6461f80f8dd14b8a15679240e651021f
Patryk Klatka <patryk.klatka@swmansion.com>
2025-01-16 15:46:11 +0100

    feat: add support for HBC bundle generation
    ## Summary
    This MR:
    1. Adds a feature to generate HBC bundles, which are well-suited for production enviornment.
    2. Modifies the `--dev` flag behavior by making the HBC bundle default when the flag is set to `false`. The HBC bundles can be disabled by setting the `--hbc-bundle` flag to false. In addition `--hermesc-options` allow for additional compilation customization. 
    
    ## Changes
    <!-- List main changes in bullet points that can be included in the changelog. Those changes should be written for
    application developers and react third-party library authors. Internal changes shouldn't be listed. Please start each bullet point with:
    - added
    - fixed
    - deprecated
    - removed
    
    For breaking changes, use "[breaking]" prefix, for example:
    - [breaking] removed something
    -->
    - added an option to generate HBC bundles in bundle-harmony CLI command
    - [breaking] bundle-harmony CLI command with --dev flag set to false will automatically generate HBC bundle 
    
    ## Test Plan
    1. Generate the .hbc bundle with `react-native bundle-harmony --dev=false`
    2. Try to load the application with the generated bundle by editing the Index.ets file
    
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    ## Issue Links
    #1442
    
    See merge request rnoh/react-native-harmony!1770
    
    Signed-off-by: huangyouhua <1356795413@qq.com>

--- react-native-harmony-cli/src/commands/bundle-harmony.ts
+++ react-native-harmony-cli/src/commands/bundle-harmony.ts
@@ -7,14 +7,23 @@ import MetroServer from 'metro/src/Server';
 import pathUtils from 'path';
 import { getAssetDestRelativePath } from '../assetResolver';
 import { ConfigT as MetroConfig } from 'metro-config';
+import { execSync } from 'child_process';
 
+const HERMESC_BIN_DIR = {
+  darwin: 'osx-bin',
+  linux: 'linux64-bin',
+  win32: 'win64-bin',
+};
+const HERMESC_PATH_PREFIX = './node_modules/react-native/sdks/hermesc/';
 const ARK_RESOURCE_PATH = './harmony/entry/src/main/resources/rawfile';
 const ASSETS_DEFAULT_DEST_PATH =
   './harmony/entry/src/main/resources/rawfile/assets';
+const TMP_BUNDLE_PATH = '/tmp/bundle.harmony.js';
 
 type AssetData = Metro.AssetData;
 type Bundle = { code: string; map: string };
 type Path = string;
+type OSType = 'darwin' | 'linux' | 'win32';
 
 export const commandBundleHarmony: Command = {
   name: 'bundle-harmony',
@@ -59,6 +68,17 @@ export const commandBundleHarmony: Command = {
       description: 'Allows overriding whether bundle is minified',
       parse: (val: string) => val !== 'false',
     },
+    {
+      name: '--hbc-bundle [boolean]',
+      description:
+        'Generates the bundle in HBC format. True by default when --dev is set to false.',
+    },
+    {
+      name: '--hermesc-options <string...>',
+      description:
+        'Additional options to pass to hermesc when generating HBC bundle. Example: --hermesc-options O g reuse-prop-cache.',
+      parse: (val, prev) => (prev ? [...prev, val] : [val]),
+    },
   ],
   func: async (argv, config, args: any) => {
     const buildOptions: BuildOptions = {
@@ -71,7 +91,15 @@ export const commandBundleHarmony: Command = {
     };
     const metroConfig = await loadMetroConfig(args.config);
     const bundle = await createBundle(metroConfig, buildOptions);
-    await saveBundle(bundle, args.bundleOutput, args.sourcemapOutput);
+    await saveBundle(
+      bundle,
+      args.bundleOutput,
+      args.sourcemapOutput,
+      args.hbcBundle !== undefined
+        ? stringToBoolean(args.hbcBundle)
+        : !args.dev,
+      args.hermescOptions
+    );
     const assets = await retrieveAssetsData(metroConfig, buildOptions);
     copyAssets(assets, args.assetsDest);
   },
@@ -94,14 +122,56 @@ async function createBundle(
   return (await Metro.runBuild(metroConfig, buildOptions)) as unknown as Bundle;
 }
 
+async function saveHBCBundle(
+  bundleCode: string,
+  bundleOutput: Path,
+  hermescOptions: string[] | undefined
+) {
+  if (
+    process.platform !== 'darwin' &&
+    process.platform !== 'linux' &&
+    process.platform !== 'win32'
+  ) {
+    throw new Error(`Unsupported platform ${process.platform} (⊙︿⊙)`);
+  }
+  const resolvedHermescPath = pathUtils.join(
+    HERMESC_PATH_PREFIX,
+    HERMESC_BIN_DIR[process.platform as OSType],
+    'hermesc'
+  );
+  await fse.ensureFile(resolvedHermescPath);
+  await fse.writeFile(TMP_BUNDLE_PATH, bundleCode);
+  const hbcFilePath = bundleOutput.endsWith('.hbc')
+    ? bundleOutput
+    : pathUtils.join(pathUtils.dirname(bundleOutput), 'hermes_bundle.hbc');
+  const hermescOptionsString =
+    hermescOptions?.map((e) => `-${e}`).join(' ') || '';
+  execSync(
+    `${resolvedHermescPath} ${hermescOptionsString} --emit-binary -out ${hbcFilePath} ${TMP_BUNDLE_PATH}`,
+    {
+      stdio: 'inherit',
+    }
+  );
+  fs.unlinkSync(TMP_BUNDLE_PATH);
+  console.log(`[CREATED] ${hbcFilePath}`);
+}
+
 async function saveBundle(
   bundle: Bundle,
   bundleOutput: Path,
-  sourceMapOutput: Path | undefined
+  sourceMapOutput: Path | undefined,
+  hbcBundle: boolean,
+  hermescOptions: string[] | undefined
 ) {
   await fse.ensureDir(pathUtils.dirname(bundleOutput));
-  fs.writeFileSync(bundleOutput, bundle.code);
-  console.log(`[CREATED] ${bundleOutput}`);
+
+  if (hbcBundle) {
+    await saveHBCBundle(bundle.code, bundleOutput, hermescOptions);
+  } else {
+    fs.writeFileSync(bundleOutput, bundle.code);
+    console.log(`[CREATED] ${bundleOutput}`);
+  }
+
   if (sourceMapOutput) {
     fs.writeFileSync(sourceMapOutput, bundle.map);
     console.log(`[CREATED] ${sourceMapOutput}`);
@@ -200,4 +270,8 @@ function copyFile(
       .pipe(fs.createWriteStream(dest))
       .on('finish', onFinished);
   });
+}
+
+function stringToBoolean(value: string): boolean {
+  return value.toLowerCase() === 'true';
 }
