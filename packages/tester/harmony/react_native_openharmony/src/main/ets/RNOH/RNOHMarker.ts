b9860bc94e5a644eb49f9b607192569ceafc28c9
Patryk Klatka <patryk.klatka@swmansion.com>
2025-02-10 16:37:11 +0100

    feat: add RNOHMarkerId enum on ArkTS side
    ## Summary
    This MR adds RNOHMarkerId enum to replicate the C++ side behavior and modifies the RNOHMarkerListener interface to have this enum as an argument.
    
    ## Changes
    - [breaking] modified RNOHMarkerListener interface to have RNOHMarkerId enum as an argument
    
    ## Test Plan
    Create simple class which inherits from `RNOHMarker` and test if `markerId` variable is an enum. 
    
    ## Checklist
    - [x] Project compiles and the tester application works.
    
    ## Issue Links
    Resolves: #1490
    
    See merge request rnoh/react-native-harmony!1805
    
    Signed-off-by: huangyouhua <1356795413@qq.com>

--- packages/tester/harmony/react_native_openharmony/src/main/ets/RNOH/RNOHMarker.ts
+++ packages/tester/harmony/react_native_openharmony/src/main/ets/RNOH/RNOHMarker.ts
@@ -11,22 +11,94 @@ export interface RNOHMarkerEventPayload {
   timestamp: number;
 }
 
+/**
+ * @api
+ */
 export interface RNOHMarkerListener {
-  logMarker(markerId: string, tag: string, timestamp: number): void;
+  logMarker(markerId: RNOHMarkerId, tag: string, timestamp: number): void;
 }
 
+/**
+ * @api
+ * Keep this enum in sync with the one defined in RNOHMarker.h
+ */
+export enum RNOHMarkerId {
+  UNKNOWN_REACT_MARKER,
+  APP_STARTUP_START,
+  APP_STARTUP_STOP,
+  INIT_REACT_RUNTIME_START,
+  INIT_REACT_RUNTIME_STOP,
+  NATIVE_REQUIRE_START,
+  NATIVE_REQUIRE_STOP,
+  RUN_JS_BUNDLE_START,
+  RUN_JS_BUNDLE_STOP,
+  CREATE_REACT_CONTEXT_START,
+  CREATE_REACT_CONTEXT_STOP,
+  JS_BUNDLE_STRING_CONVERT_START,
+  JS_BUNDLE_STRING_CONVERT_STOP,
+  NATIVE_MODULE_SETUP_START,
+  NATIVE_MODULE_SETUP_STOP,
+  REGISTER_JS_SEGMENT_START,
+  REGISTER_JS_SEGMENT_STOP,
+  REACT_INSTANCE_INIT_START,
+  REACT_INSTANCE_INIT_STOP,
+  CONTENT_APPEARED,
+  DOWNLOAD_START,
+  DOWNLOAD_END,
+  REACT_BRIDGE_LOADING_START,
+  REACT_BRIDGE_LOADING_END,
+  ON_HOST_RESUME_START,
+  ON_HOST_RESUME_END,
+  ON_HOST_PAUSE_START,
+  ON_HOST_PAUSE_END,
+  CHANGE_THREAD_PRIORITY,
+  INITIALIZE_MODULE_START,
+  INITIALIZE_MODULE_END,
+  PROCESS_PACKAGES_START,
+  PROCESS_PACKAGES_END,
+  CREATE_UI_MANAGER_MODULE_START,
+  CREATE_UI_MANAGER_MODULE_END,
+  CREATE_MODULE_START,
+  CREATE_MODULE_END,
+  PROCESS_CORE_REACT_PACKAGE_START,
+  PROCESS_CORE_REACT_PACKAGE_END,
+  FABRIC_COMMIT_START,
+  FABRIC_COMMIT_END,
+  FABRIC_FINISH_TRANSACTION_START,
+  FABRIC_FINISH_TRANSACTION_END,
+  FABRIC_DIFF_START,
+  FABRIC_DIFF_END,
+  FABRIC_LAYOUT_START,
+  FABRIC_LAYOUT_END,
+  FABRIC_BATCH_EXECUTION_START,
+  FABRIC_BATCH_EXECUTION_END,
+  FABRIC_UPDATE_UI_MAIN_THREAD_START,
+  FABRIC_UPDATE_UI_MAIN_THREAD_END,
+}
+
+/**
+ * @api
+ */
 export class RNOHMarker {
-  private static listeners: RNOHMarkerListener[] = [];
+  private static listeners: Set<RNOHMarkerListener> = new Set();
 
   static addListener(listener: RNOHMarkerListener) {
-    this.listeners.push(listener);
+    this.listeners.add(listener);
   }
 
   static removeListener(listener: RNOHMarkerListener) {
-    this.listeners = this.listeners.filter((l) => l != listener);
+    this.listeners.delete(listener);
   }
 
+  /**
+   * @internal
+   */
   static notifyListeners(markerId: string, tag: string, timestamp: number) {
-    this.listeners.forEach((listener) => listener.logMarker(markerId, tag, timestamp))
+    const resolvedMarkerId =
+      RNOHMarkerId[markerId as keyof typeof RNOHMarkerId] ||
+      RNOHMarkerId.UNKNOWN_REACT_MARKER;
+    this.listeners.forEach(listener =>
+      listener.logMarker(resolvedMarkerId, tag, timestamp),
+    );
   }
 }
