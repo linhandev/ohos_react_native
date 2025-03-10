61b34b5234d6161a6f702f7536ffb167f608d379
Wojciech Łoboda <wojciech.loboda@swmansion.com>
2025-03-10 18:31:13 +0100

    chore: Update react-native-core to 0.77.1
    ## Changes
    <!-- REMOVE THIS COMMENT BEFORE MERGING TO AVOID INCLUDING IT IN GIT HISTORY 
    
    List changes in bullet points that should be included in the changelog. Those changes should be written for
    application developers and react-native third-party library maintainers. Internal changes shouldn't be listed. Please start each bullet point with:
    - added
    - fixed
    - deprecated
    - removed
    
    For breaking changes, use "[breaking]" prefix, for example:
    - [breaking] removed xyz
    -->
    - update and react-native to 0.77.1
    - changed react-native-core submodule branch to 0.77.1
    ## Test Plan
    
    ## Checklist
    - [x] `pnpm verify` passes
    - [x] no test fails on `CONCURRENT TESTER` page in tester app
    - [x] no test fails on `SEQUENTIAL TESTER` page in tester app
    
    ## Issue Links
    Closes: https://gl.swmansion.com/rnoh/react-native-harmony/-/issues/1516
    
    See merge request rnoh/react-native-harmony!1863
    
    Signed-off-by: huangyouhua <1356795413@qq.com>

--- packages/react-native-harmony/scripts/build-hermes.sh
+++ packages/react-native-harmony/scripts/build-hermes.sh
@@ -8,7 +8,7 @@ fi
 HERMES_REPO_URL="https://github.com/facebook/hermes.git"
 
 # NOTE: these variables should be kept in sync with the React Native version RNOH is based off of
-REACT_NATIVE_VERSION="0.77.0"
+REACT_NATIVE_VERSION="0.77.1"
 HERMES_TAG="hermes-2024-11-25-RNv0.77.0-d4f25d534ab744866448b36ca3bf3d97c08e638c"
 
 SCRIPT_PATH=$(realpath "$0")
