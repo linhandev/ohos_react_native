d1fb6f2ef9ddfb40d8b1dd41ac65d63d33f377fc
Patryk Klatka <patryk.klatka@gmail.com>
2025-03-17 22:07:33 +0100

    fix: build-har script
    Signed-off-by: Patryk Klatka <patryk.klatka@gmail.com>

--- packages/react-native-harmony/scripts/build-har.js
+++ packages/react-native-harmony/scripts/build-har.js
@@ -1,45 +1,51 @@
 // @ts-check
-const pathUtils = require('node:path');
-const { execSync } = require('node:child_process');
-const fs = require('node:fs');
+const { execSync } = require("node:child_process");
+const fs = require("node:fs");
+const { glob } = require("glob");
+const pathUtils = require("node:path");
 
-const DEVECO_SDK_HOME = process.env.DEVECO_SDK_HOME;
-
-if (!DEVECO_SDK_HOME) {
-  console.error('DEVECO_SDK_HOME is undefined');
-  process.exit(1);
-}
-
-const nodePath = pathUtils.join(
-  DEVECO_SDK_HOME,
-  '..',
-  'tools',
-  'node',
-  'bin',
-  'node'
-);
-
-const hvigorwPath = pathUtils.join(
-  DEVECO_SDK_HOME,
-  '..',
-  'tools',
-  'hvigor',
-  'bin',
-  'hvigorw.js'
-);
-
-try {
-  const buildHarCommand = `"${nodePath}" "${hvigorwPath}" --mode module -p product=default -p module=react_native_openharmony@default -p buildMode=release assembleHar --analyze=normal --parallel --incremental --daemon`;
-  execSync(buildHarCommand, { stdio: 'inherit', cwd: '../tester/harmony' });
-  if (!fs.existsSync('./harmony')) {
-    fs.mkdirSync('./harmony/');
+/**
+ * @param {string} directory
+ * @param {string} filename
+ */
+async function findFirstFile(directory, filename) {
+  try {
+    const files = await glob(`${directory}/**/${filename}`, { nodir: true });
+    return files.length > 0 ? files[0] : null;
+  } catch (err) {
+    console.error(`Error searching for ${filename}: ${err}`);
+    return null;
   }
-  fs.copyFileSync(
-    '../tester/harmony/react_native_openharmony/build/default/outputs/default/react_native_openharmony.har',
-    './harmony/react_native_openharmony.har'
-  );
-  process.exit(0);
-} catch (error) {
-  console.error('Command execution failed:', error);
-  process.exit(1);
 }
+
+(async () => {
+  const DEVECO_SDK_HOME = process.env.DEVECO_SDK_HOME;
+
+  if (!DEVECO_SDK_HOME) {
+    console.error("DEVECO_SDK_HOME is undefined");
+    process.exit(1);
+  }
+
+  const basePath = pathUtils.join(DEVECO_SDK_HOME, "..");
+  const nodePath = await findFirstFile(
+    basePath,
+    "node" + (process.platform === "win32" ? ".exe" : "")
+  );
+  const hvigorwPath = await findFirstFile(basePath, "hvigorw.js");
+
+  try {
+    const buildHarCommand = `"${nodePath}" "${hvigorwPath}" --mode module -p product=default -p module=react_native_openharmony@default -p buildMode=release assembleHar --analyze=normal --parallel --incremental --daemon`;
+    execSync(buildHarCommand, { stdio: "inherit", cwd: "../tester/harmony" });
+    if (!fs.existsSync("./harmony")) {
+      fs.mkdirSync("./harmony/");
+    }
+    fs.copyFileSync(
+      "../tester/harmony/react_native_openharmony/build/default/outputs/default/react_native_openharmony.har",
+      "./harmony/react_native_openharmony.har"
+    );
+    process.exit(0);
+  } catch (error) {
+    console.error("Command execution failed:", error);
+    process.exit(1);
+  }
+})();
