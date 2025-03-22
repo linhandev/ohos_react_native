e052a9cc2d1493a77ed0534601c0dc40a4a21d2e
pklatkaa <patryk.klatka@gmail.com>
2025-03-22 11:56:48 +0800

    chore: rewrite formatting scripts
    # message auto-generated for no-merge-commit merge:
    merge @pklatka/format-scripts into 0.77.1-rc.1-ohos
    
    chore: rewrite formatting scripts
    
    Created-by: pklatkaa
    Commit-by: Patryk Klatka
    Merged-by: my_performance
    Description: ## Why are these changes being made?
    This PR rewrites formatting scripts to be compatible with Windows platform.
    
    ## Test Plan
    Run `format:cpp` and `format:cpp:check` scripts.
    
    ## Checklist
    - [x] `pnpm verify` command has passed successfully.
    - [x] No failures occurred in automatic tests on the `CONCURRENT TESTER` page in the tester app.
    - [x] No failures occurred in automatic tests on the `SEQUENTIAL TESTER` page in the tester app.
    - [x] MR does not include a breaking change or a review has been requested from a Huawei reviewer.
    
    ## Issue links
    https://gitcode.com/openharmony-sig/ohos_react_native/issues/414
    
    See merge request: openharmony-sig/ohos_react_native!665

---
+++ scripts/format-cpp.ts
@@ -0,0 +1,134 @@
+import { exec, execSync } from "node:child_process";
+import { glob } from "glob";
+import pathUtils from "node:path";
+
+if (require.main === module) {
+  (async () => {
+    try {
+      if (process.argv.length > 2 && process.argv[2] === "--check") {
+        await checkClangFormat();
+      } else {
+        await clangFormat();
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  })();
+}
+
+async function checkClangFormat(): Promise<void> {
+  try {
+    const clangFormatPath = await getClangFormatPath();
+    const changedFiles = await findChangedCppFiles();
+    if (changedFiles.size === 0) {
+      return;
+    }
+    const command = `${clangFormatPath} --dry-run --Werror --style=file:.clang-format ${stringifyFilePaths(changedFiles)}`;
+    try {
+      execSync(command, { stdio: "inherit" });
+    } catch (error) {
+      console.error("Error: clang-format check failed");
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function clangFormat(): Promise<void> {
+  try {
+    const clangFormatPath = await getClangFormatPath();
+    const changedFiles = await findChangedCppFiles();
+    if (changedFiles.size === 0) {
+      return;
+    }
+    const command = `${clangFormatPath} -i ${stringifyFilePaths(changedFiles)} --style=file:.clang-format`;
+    try {
+      execSync(command, { stdio: "inherit" });
+    } catch (error) {
+      console.error("Error: clang-format check failed");
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+async function getClangFormatPath(): Promise<string> {
+  const sdkHome = process.env.DEVECO_SDK_HOME;
+  if (!sdkHome) {
+    console.error("DEVECO_SDK_HOME environment variable is not set");
+    process.exit(1);
+  }
+  const clangFormatPathCandidates = await glob(
+    `${pathUtils.join(sdkHome, "..")}/**/clang-format${process.platform === "win32" ? ".exe" : ""}`,
+    { nodir: true }
+  );
+  if (clangFormatPathCandidates.length === 0) {
+    console.error("clang-format not found in DEVECO_SDK_HOME");
+    process.exit(1);
+  }
+  return clangFormatPathCandidates[0];
+}
+
+export async function findChangedCppFiles(): Promise<Set<string>> {
+  const changedFiles = await findChangedFiles();
+  for (const path of changedFiles) {
+    if (
+      (!path.endsWith(".cpp") && !path.endsWith(".h")) ||
+      path.includes("third-party") ||
+      path.includes("generated") ||
+      path.includes("patches")
+    ) {
+      changedFiles.delete(path);
+    }
+  }
+  return changedFiles;
+}
+
+export async function findChangedFiles(): Promise<Set<string>> {
+  try {
+    const currentBranch = execSync("git branch --show-current")
+      .toString()
+      .trim();
+    const revList = execSync(
+      `git rev-list --exclude ${currentBranch} --branches -1`
+    )
+      .toString()
+      .trim();
+    const diffFromBranchCmd = `git diff --name-only --diff-filter=d ${revList} HEAD`;
+    const diffUnstagedCmd = "git diff --name-only --diff-filter=d";
+    const diffStagedCmd = "git diff --name-only --diff-filter=d --cached";
+    const result = await Promise.all([
+      execGitDiffCommand(diffFromBranchCmd),
+      execGitDiffCommand(diffUnstagedCmd),
+      execGitDiffCommand(diffStagedCmd),
+    ]);
+    return new Set(result.flat());
+  } catch (error) {
+    console.error(error);
+    return new Set();
+  }
+}
+
+async function execGitDiffCommand(command: string): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      const files = stdout.trim().split("\n").filter(Boolean);
+      resolve(files);
+    });
+  });
+}
+
+function stringifyFilePaths(changedFiles: Set<string>): string {
+  let filesStringified = "";
+  for (const file of changedFiles) {
+    filesStringified += file;
+  }
+  return filesStringified;
+}
