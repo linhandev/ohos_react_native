b797740acbaedcd49277be536be7d1629b2a006e
linhandev <hanlin21@huawei.com>
2025-03-26 20:23:55 +0800

    chore: switch to gitcode mirror to speed up library download
    Signed-off-by: linhandev <hanlin21@huawei.com>
    
    
    # message auto-generated for no-merge-commit merge:
    merge gitcode into 0.77.1-rc.1-ohos
    
    chore: switch to download dependencies from gitcode to speedup download
    
    Created-by: linhandev
    Commit-by: linhandev
    Merged-by: huangyouhua
    Description: ## Description
    For most users of this project, pulling from github without special networksetup is very slow and may fail. Using gitcode mirror speeds up pulling process.
    
    Note: we haven't figured out a better way than downloading boost artifact from github so that one isn't changed. We'll continue to investigate
    
    ## Changes
    
    ## Test Plan
    
    Delete the .cxx and build folder under entry or do a fresh clone. Build process should succeed.
    
    
    ## Checklist
    - [x] `pnpm verify` command has passed successfully.
    - [x] No failures occurred in automatic tests on the `CONCURRENT TESTER` page in the tester app.
    - [x] No failures occurred in automatic tests on the `SEQUENTIAL TESTER` page in the tester app.
    - [x] MR does not include a breaking change or a review has been requested from a Huawei reviewer.
    
    
    See merge request: openharmony-sig/ohos_react_native!702

--- packages/tester/harmony/react_native_openharmony/src/main/cpp/third-party/FetchDependencies.cmake
+++ packages/tester/harmony/react_native_openharmony/src/main/cpp/third-party/FetchDependencies.cmake
@@ -5,7 +5,7 @@ message("Fetching third-party dependencies, this may take a while...")
 # FMT
 FetchContent_Declare(
   Fmt
-  GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+  GIT_REPOSITORY https://gitcode.com/gh_mirrors/fm/fmt.git
   GIT_TAG 0c9fce2ffefecfdce794e1859584e25877b7b592 # 11.0.2
   GIT_SHALLOW TRUE
   OVERRIDE_FIND_PACKAGE)
@@ -13,7 +13,7 @@ FetchContent_Declare(
 # DOUBLE CONVERSION
 FetchContent_Declare(
   DoubleConversion
-  GIT_REPOSITORY https://github.com/google/double-conversion.git
+  GIT_REPOSITORY https://gitcode.com/gh_mirrors/dou/double-conversion.git
   GIT_TAG 4f7a25d8ced8c7cf6eee6fd09d6788eaa23c9afe # v3.3.0
   GIT_SHALLOW TRUE
   OVERRIDE_FIND_PACKAGE)
@@ -21,7 +21,7 @@ FetchContent_Declare(
 # GLOG
 FetchContent_Declare(
   Glog
-  GIT_REPOSITORY https://github.com/google/glog.git
+  GIT_REPOSITORY https://gitcode.com/gh_mirrors/glog6/glog.git
   GIT_TAG a6a166db069520dbbd653c97c2e5b12e08a8bb26 # v0.3.5
   GIT_SHALLOW TRUE
   OVERRIDE_FIND_PACKAGE)
@@ -42,7 +42,7 @@ set(EVENT__DISABLE_OPENSSL
     CACHE BOOL "" FORCE)
 FetchContent_Declare(
   LibEvent
-  GIT_REPOSITORY https://github.com/libevent/libevent.git
+  GIT_REPOSITORY https://gitcode.com/gh_mirrors/li/libevent.git
   GIT_TAG 5df3037d10556bfcb675bc73e516978b75fc7bc7 # release-2.1.12-stable
   GIT_SHALLOW TRUE
   OVERRIDE_FIND_PACKAGE)
@@ -50,7 +50,7 @@ FetchContent_Declare(
 # HERMES
 FetchContent_Declare(
   Hermes
-  GIT_REPOSITORY https://github.com/facebook/hermes.git
+  GIT_REPOSITORY https://gitcode.com/gh_mirrors/hermes/hermes.git
   GIT_TAG
     d4f25d534ab744866448b36ca3bf3d97c08e638c # hermes-2024-11-25-RNv0.77.0-d4f25d534ab744866448b36ca3bf3d97c08e638c
   GIT_SHALLOW TRUE
@@ -60,7 +60,7 @@ FetchContent_Declare(
 # FOLLY
 FetchContent_Declare(
   Folly
-  GIT_REPOSITORY https://github.com/facebook/folly.git
+  GIT_REPOSITORY https://gitcode.com/gh_mirrors/fol/folly.git
   GIT_TAG 0660ead5b610047a279cc291129e98e3f2a1fc37 # v2024.11.18.00
   GIT_SHALLOW TRUE
   SOURCE_SUBDIR "__none__" # ignore folly's CMakeLists.txt
@@ -69,7 +69,7 @@ FetchContent_Declare(
 # FAST_FLOAT
 FetchContent_Declare(
   FastFloat
-  GIT_REPOSITORY https://github.com/fastfloat/fast_float.git
+  GIT_REPOSITORY https://gitcode.com/gh_mirrors/fa/fast_float.git
   GIT_TAG 3e57d8dcfb0a04b5a8a26b486b54490a2e9b310f # v6.1.4
   GIT_SHALLOW TRUE
   OVERRIDE_FIND_PACKAGE
