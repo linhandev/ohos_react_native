8bb57f5b4724e085014cae6eeab711f284010ac8
linhandev <hanlin21@huawei.com>
2025-04-24 20:31:52 +0800

    chore: add license scanning config, change some licence headers
    # message auto-generated for no-merge-commit merge:
    merge license into 0.77.1-rc.1-ohos
    
    chore: add license scanning config, change some licence headers
    
    Created-by: linhandev
    Commit-by: linhandev
    Merged-by: huangyouhua
    Description: ## Why are these changes being made?
    
    - Add license header scanning config. Without that oat config codecheck task in start build would fail while adding new source code file.
    
    - Config files don't usually get license headers, but since clang configs takes comments, I'd like to add license headers in them to be on the safe side. pnpm verify and format both works after this change.
    
    - Our jsvm related code license header is referencing non existing LICENSE-MIT file, changed that to LICENSE
    
    - RNTypes.ts is mainly copied from upstream, it should use meta's license
    
    ## Changelog
    
    ## Test Plan
    
    Add a new source code file with hw license header, ci should pass
    
    ## Checklist
    - [x] `pnpm verify` command has passed successfully.
    ~~- [ ] No failures occurred in automatic tests on the `CONCURRENT TESTER` page in the tester app.~~
    ~~- [ ] No failures occurred in automatic tests on the `SEQUENTIAL TESTER` page in the tester app.~~
    - [x] MR does not include a breaking change or a review has been requested from a Huawei reviewer.
    
    
    See merge request: openharmony-sig/ohos_react_native!862

--- packages/tester/harmony/react_native_openharmony/src/main/ets/RNOH/RNTypes.ts
+++ packages/tester/harmony/react_native_openharmony/src/main/ets/RNOH/RNTypes.ts
@@ -1,11 +1,4 @@
 /**
- * Copyright (c) 2024 Huawei Technologies Co., Ltd.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-/**
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
