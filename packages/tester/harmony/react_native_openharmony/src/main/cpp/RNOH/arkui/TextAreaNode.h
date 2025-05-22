93b63a754b274389c20e5b5ec839559a3dd96c3d
zhoukc42 <1540623907@qq.com>
2025-05-22 20:03:03 +0800

    doc: for TextArea
    Co-authored-by: zhoukc42<1540623907@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge TextArea-note into 0.77.1-rc.1-ohos
    
    doc: for TextArea
    
    Created-by: zhoukc42
    Commit-by: zhoukc42
    Merged-by: huangyouhua
    Description: <!-- 请在合并之前删除注释代码，以避免将其包含在GIT历史记录中 -->
    # Why are these changes being made?
    <!--
    简要描述PR的目的，例如:
    这条PR主要实现metro局域网内远程调试的功能. 
    -->
    Add doc for TextArea.
    
    # Changelog
    <!--
    列出可包含在变更日志中的要点的主要变更。
    不要列出内部更改。请以下列开头来写每个变更点：
    - added
    - fixed
    - deprecated
    - removed
    如果是一条新增属性的变更，则以[added]开头，例如:
    - [added] FlatList组件新增inverted属性
    -->
    [added] add doc for TextArea.
    
    # Test Plan
    <!-- 
    如何测试这条PR。例如：
    1. 打开Tester工程.
    2. 执行npm run start命令.
    ...
    -->
    1. Check the compilation status of Tester.
    
    # Checklist
    <!--
    请在合入前根据以下条例进行自检
    自检完成后在[ ]内填入"x", 例如：
    - [x] 不涉及非兼容性变更；若涉及，已通过相应评审。
    -->
    - [x] Does not involve incompatible changes; if involved, has been reviewed accordingly.
    - [x] Does not impact performance, or performance testing has been conducted without degradation.
    - [x] Complies with the relevant coding standards.
    - [x] Does not involve documentation updates, or the documentation has been updated.
    - [x] Meets testability requirements with necessary self-test cases, appropriate logging, or trace information added.
    - [x] No illegal file inclusions exist, such as images or code.
    
    See merge request: openharmony-sig/ohos_react_native!1043

--- packages/tester/harmony/react_native_openharmony/src/main/cpp/RNOH/arkui/TextAreaNode.h
+++ packages/tester/harmony/react_native_openharmony/src/main/cpp/RNOH/arkui/TextAreaNode.h
@@ -126,40 +126,111 @@ class TextAreaNode : public TextInputNodeBase {
   TextAreaNode();
   ~TextAreaNode() override;
 
+  /**
+   * @brief Obtains the offset of TextArea.
+   * @return The offset of TextArea.
+   */
   facebook::react::Point getTextAreaOffset() const;
 
+  /**
+   * @brief Obtains the position of the edited text area relative to the
+   * component and its size.
+   * @return The position of the edited text area relative to the component
+   * and its size.
+   */
   facebook::react::Rect getTextContentRect() const override;
 
+  /**
+   * @brief The callback function to receive component events.
+   * @param eventType The event type of ArkUI.
+   * @param eventArgs The parameter of the component callback event.
+   */
   void onNodeEvent(ArkUI_NodeEventType eventType, EventArgs& eventArgs)
       override;
 
+  /**
+   * @brief The callback function to receive component events.
+   * @param eventType The event type of ArkUI.
+   * @param eventString The string data in a component event.
+   */
   void onNodeEvent(ArkUI_NodeEventType eventType, std::string_view eventString)
       override;
 
+  /**
+   * @brief Assigns a delegate to handle TextArea node events.
+   * @param textAreaNodeDelegate The delegate object (must implement
+   * TextAreaNodeDelegate).
+   */
   void setTextAreaNodeDelegate(TextAreaNodeDelegate* textAreaNodeDelegate);
 
+  /**
+   * @brief Set the text content of TextArea.
+   * @param textContent Text content.
+   */
   void setTextContent(std::string const& textContent);
 
+  /**
+   * @brief Set the text area type.
+   * @param keyboardType Determines which keyboard to open.
+   */
   void setInputType(facebook::react::KeyboardType keyboardType);
 
+  /**
+   * @brief Set the text area type.
+   * @param keyboardType Determines which keyboard to open.
+   */
   void setInputType(ArkUI_TextAreaType keyboardType);
 
+  /**
+   * @brief Set the font style for the TextArea.
+   * @param textAttributes TextAttributes object.
+   */
   void setFont(facebook::react::TextAttributes const& textAttributes) override;
 
+  /**
+   * @brief Set the caret color.
+   * @param color The caret color.
+   */
   void setCaretColor(facebook::react::SharedColor const& color) override;
 
+  /**
+   * @brief Set the maximum number of characters in the text area.
+   * @param maxLength The maximum number of characters in the text area.
+   */
   void setMaxLength(int32_t maxLength) override;
 
+  /**
+   * @brief Set the placeholder text of the TextArea.
+   * @param placeholder The placeholder text.
+   */
   void setPlaceholder(std::string const& placeholder) override;
 
+  /**
+   * @brief Set the placeholder text color of the TextArea.
+   * @param color The placeholder text color.
+   */
   void setPlaceholderColor(facebook::react::SharedColor const& color) override;
 
+  /**
+   * @brief Set default Padding.
+   */
   void defaultSetPadding();
 
+  /**
+   * @brief Sets the autofill type.
+   * @param textContentType
+   */
   void setTextContentType(std::string const& textContentType);
 
-  void setContextMenuHidden(bool hidden);
   /**
+   * @brief Sets whether to hide the text selection menu when the text box is
+   * long-pressed, double-click, or right-clicked.
+   * @param hidden If true, context menu is hidden.
+   */
+  void setContextMenuHidden(bool hidden);
+
+  /**
+   * @brief Sets whether to hide the text selection menu when the text box is
    * @Deprecated: use setContextMenuHidden.
    * Deprecated when preparing 0.77 for release.
    */
@@ -167,18 +238,44 @@ class TextAreaNode : public TextInputNodeBase {
     this->setContextMenuHidden(hidden);
   }
 
+  /**
+   * @brief Sets whether the TextArea loses focus after the Enter key is
+   * pressed to submit information.
+   * @param blurOnSubmit If true, the TextArea will loss focus.
+   */
   void setBlurOnSubmit(bool blurOnSubmit);
 
+  /**
+   * @brief Set the text line height attribute.
+   * @param lineHeight
+   */
   void setLineHeight(float lineHeight);
 
+  /**
+   * @brief Set the type of the Enter key.
+   * @param returnKeyType Determines how the return key should look.
+   * @param returnKeyLabel Sets the return key to the label when `returnKeyType`
+   * is set to `default`.
+   */
   void setEnterKeyType(
       facebook::react::ReturnKeyType returnKeyType,
       std::string returnKeyLabel);
 
+  /**
+   * @brief Set the type of the Enter key.
+   * @param returnKeyType Determines how the return key should look.
+   */
   void setEnterKeyType(ArkUI_EnterKeyType returnKeyType);
 
-  void setUnderlineColor(facebook::react::SharedColor const& underlineColor);
   /**
+   * @brief Sets the color of the text underline.
+   * @param underlineColor
+   */
+  void setUnderlineColor(facebook::react::SharedColor const& underlineColor);
+
+  /**
+   * @brief Sets the color of the text underline.
+   * @param underlineColorAndroid
    * @Deprecated: use setUnderlineColor.
    * Deprecated when preparing 0.77 for release.
    */
@@ -187,12 +284,30 @@ class TextAreaNode : public TextInputNodeBase {
     this->setUnderlineColor(underlineColorAndroid);
   };
 
+  /**
+   * @brief Sets whether to enable autofill.
+   * @param autoFill whether to enable autofill.
+   */
   void setAutoFill(bool autoFill);
 
+  /**
+   * @brief Sets whether to enable autofill.
+   * @param autoFill Tells the operating system whether the individual fields
+   * in your app should be included in a view structure for autofill purposes.
+   * Possible values are auto, no, noExcludeDescendants, yes, and
+   * yesExcludeDescendants. The default value is auto.
+   */
   void setAutoFill(std::string const& autoFill);
 
-  void setShowKeyboardOnFocus(bool enable);
   /**
+   * @brief Sets whether the keyboard pops up when the input box gains focus.
+   * @param enable When false, it will prevent the soft keyboard from showing
+   * when the field is focused.
+   */
+  void setShowKeyboardOnFocus(bool enable);
+
+  /**
+   * @brief Sets whether the keyboard pops up when the input box gains focus.
    * @Deprecated: use setShowKeyboardOnFocus
    * Deprecated when preparing 0.77 branch for release.
    */
@@ -200,18 +315,46 @@ class TextAreaNode : public TextInputNodeBase {
     this->setShowKeyboardOnFocus(enable);
   }
 
+  /**
+   * @brief Sets the regular expression for input filtering.
+   * @param inputFilter The regular expression.
+   */
   void setInputFilter(std::string const& inputFilter);
 
+  /**
+   * @brief Set whether the input box automatically gets focus.
+   * @param autoFocus When autoFocus is true, the input box automatically
+   * gets focus.
+   */
   void setAutoFocus(bool autoFocus);
 
+  /**
+   * @brief Obtains text content of the TextArea.
+   * @return Text content.
+   */
   std::string getTextContent() override;
 
+  /**
+   * @brief Convert TextArea EnterKeyType
+   * @param returnKeyType Enter key type enumeration
+   * @return Type of enter key in ArkUI
+   */
   static ArkUI_EnterKeyType convertTextAreaEnterKeyType(
       facebook::react::ReturnKeyType returnKeyType);
 
+  /**
+   * @brief Convert TextArea EnterKeyType
+   * @param returnKeyType Enter key type string
+   * @return Type of enter key in ArkUI
+   */
   static ArkUI_EnterKeyType convertTextAreaEnterKeyLabel(
       std::string returnKeyLabel);
 
+  /**
+   * @brief Convert TextArea Keyboard type
+   * @param keyboardType Keyboard type enumeration
+   * @return TextArea keyboard type in ArkUI
+   */
   static ArkUI_TextAreaType convertTextAreaInputType(
       facebook::react::KeyboardType keyboardType);
 };
