8ac36f1eeb24189529c0cc2430d6a2544536c30d
SosenWiosen <przemek.sosna@onet.pl>
2025-06-12 18:12:19 +0800

    Fix: KeyboardAvoidingView jitters when it takes up the whole screen
    Co-authored-by: Przemysław Sosna<przemyslaw.sosna@swmansion.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge @sosen/keyboard-avoiding-view-fix into 0.77.1-rc.1-ohos
    
    Fix: KeyboardAvoidingView jitters when it takes up the whole screen
    
    Created-by: SosenWiosen
    Commit-by: Przemysław Sosna
    Merged-by: huangyouhua
    Description: ## Description
    In certain situations, for example when we have a KeyboardAvoidingView set to height inside a modal component, it can jitter. This occurs after the keyboard disappears.
    This occurs on 0.77 but not on 0.72. The bug is caused by a combination of two things:
    1. Our turbomodules are implemented in ArkTS. Js handles all numbers as double precision floats, so when the keyboard hides, we pass endScreenY to react-native as a double. Yoga uses floats
    2.  In KeyboardAvoidingView.js relativeKeyboardHeight is calculated as `Math.max(this.state.bottom + frame.y + frame.height - keyboardY, 0);`. `frame` is obtained through the onLayout event. When the KeyboardAvoidingView takes up the whole screen, `frame.height `will be the height of the whole screen. WIth the keyboard hidden, `keyboardY` should albo be the height of the whole screen.`this.state.bottom + frame.y` is equal zero. Let's assume the size of the screen `px2vp(windowRect.height)` is 801.28 (Huawei X5). Because yoga uses single precision floats, `frame.height` will be actually 801.280029296875. Therefore, relativeKeyboardHeight will be non zero, leading to an endless loop of re-renders.
    
    This didn't occur on 0.72 because the renderer behaviour was different. I checked this bug on android, and if the frame.height and keyboardY don't match, it behaves the same.
    
    ## Changes
    - fix: round values in KeyboardObserverTurboModule to floats,
    - fix: make the event sent by KeyboardObserverTurboModule more inline with 0.72 and other platforms,
    
    ## Test Plan
    
    
    
    ## Checklist
    - [x] Does not involve incompatible changes; if involved, has been reviewed accordingly.
    - [x] Does not impact performance, or performance testing has been conducted without degradation.
    - [x] Complies with the relevant coding standards.
    - [x] Does not involve documentation updates, or the documentation has been updated.
    - [x] Meets testability requirements with necessary self-test cases, appropriate logging, or trace information added.
    - [x] No illegal file inclusions exist, such as images or code.
    
    
    See merge request: openharmony-sig/ohos_react_native!1134

--- packages/tester/harmony/react_native_openharmony/src/main/ets/RNOHCorePackage/turboModules/KeyboardObserverTurboModule.ts
+++ packages/tester/harmony/react_native_openharmony/src/main/ets/RNOHCorePackage/turboModules/KeyboardObserverTurboModule.ts
@@ -13,21 +13,49 @@ declare function px2vp(px: number): number;
 
 export class KeyboardObserverTurboModule extends TurboModule {
   public static readonly NAME = 'KeyboardObserver';
+  private keyboardHeight: number;
 
   constructor(protected ctx: TurboModuleContext) {
     super(ctx);
     this.subscribeListeners();
   }
 
-  private createKeyboardEvent(screenX: number, screenY: number, width: number, height: number) {
+  private createKeyboardEvent({
+    screenX,
+    startScreenY,
+    endScreenY,
+    startHeight,
+    endHeight,
+    width,
+  }: {
+    screenX: number;
+    startScreenY: number;
+    endScreenY: number;
+    startHeight: number;
+    endHeight: number;
+    width: number;
+  }) {
+    /*
+    * KeyboardAvoidingView calculates the relativeKeyboardHeight based on the height of the component received
+    * from onLayout, which in some cases can be the whole screen. Yoga uses floats for layout, which leads to
+    * rounding errors. Passing the below numbers without any rounding (JS numbers are double precision)
+    * can lead to relativeKeyboardHeight being calculated as non-zero when the keyboard is hidden,
+    * leading to an infinite loop of re-renders and jittering.
+    */
     return {
       easing: 'keyboard',
       duration: 0,
       endCoordinates: {
-        screenX: px2vp(screenX),
-        screenY: px2vp(screenY),
-        height: px2vp(height),
-        width: px2vp(width),
+        screenX: Math.fround(px2vp(screenX)),
+        screenY: Math.fround(px2vp(endScreenY)),
+        height: Math.fround(px2vp(endHeight)),
+        width: Math.fround(px2vp(width)),
+      },
+      startCoordinates: {
+        screenX: Math.fround(px2vp(screenX)),
+        screenY: Math.fround(px2vp(startScreenY)),
+        height: Math.fround(px2vp(startHeight)),
+        width: Math.fround(px2vp(width)),
       }
     }
   }
@@ -37,15 +65,35 @@ export class KeyboardObserverTurboModule extends TurboModule {
     // using inputMethodEngine.on('keyboardShow') and .on('keyboardHide') would be preferable, but it doesn't work at the time of writing.
     windowInstance.on('keyboardHeightChange', async (keyboardHeight) => {
       if (keyboardHeight > 0) {
+        const windowRect = windowInstance.getWindowProperties().windowRect;
         const keyboardAvoidArea = windowInstance.getWindowAvoidArea(window.AvoidAreaType.TYPE_KEYBOARD).bottomRect;
-        this.ctx.rnInstance.emitDeviceEvent('keyboardDidShow', this.createKeyboardEvent(keyboardAvoidArea.left, keyboardAvoidArea.top, keyboardAvoidArea.width, keyboardAvoidArea.height));
-        this.ctx.rnInstance.postMessageToCpp("KEYBOARD_VISIBLE", {keyboardHeight: px2vp(keyboardAvoidArea.height)});
+        this.keyboardHeight = keyboardAvoidArea.height;
+        this.ctx.rnInstance.emitDeviceEvent('keyboardDidShow',
+          this.createKeyboardEvent({
+            screenX: keyboardAvoidArea.left,
+            startScreenY: windowRect.height,
+            endScreenY: keyboardAvoidArea.top,
+            startHeight: 0,
+            endHeight: keyboardHeight,
+            width: windowRect.width,
+          }));
+        this.ctx.rnInstance.postMessageToCpp("KEYBOARD_VISIBLE", { keyboardHeight: px2vp(keyboardAvoidArea.height) });
       } else {
         const windowRect = windowInstance.getWindowProperties().windowRect;
-        this.ctx.rnInstance.emitDeviceEvent('keyboardDidHide', this.createKeyboardEvent(0, windowRect.height, windowRect.width, 0))
+        const keyboardAvoidArea = windowInstance.getWindowAvoidArea(window.AvoidAreaType.TYPE_KEYBOARD).bottomRect;
+        this.ctx.rnInstance.emitDeviceEvent('keyboardDidHide',
+          this.createKeyboardEvent({
+            screenX: keyboardAvoidArea.left,
+            startScreenY: windowRect.height - this.keyboardHeight,
+            endScreenY: windowRect.height,
+            startHeight: this.keyboardHeight,
+            endHeight: 0,
+            width: windowRect.width,
+          }));
       }
-    })
+    }
 
+    )
   }
 }
 
