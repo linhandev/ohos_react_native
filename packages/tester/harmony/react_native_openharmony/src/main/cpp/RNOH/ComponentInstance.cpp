af594db33f43cb4f0ea2e36e3d3cf45484682a2d
xiedrsz <xiedairong@huawei.com>
2025-10-18 16:57:46 +0800

    fix: Supplement null pointer protection.
    Co-authored-by: 谢代荣<xiedairong@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1687 merge 77-insertChild into 0.77.1-rc.1-ohos
    
    fix: Supplement null pointer protection.
    
    Created-by: xiedrsz
    Commit-by: 谢代荣
    Merged-by: huangyouhua
    Description: ## Why are these changes being made?
    A partner detected that executing insertChild could cause a null pointer crash. Upon reviewing the code, it was found that childComponentInstance already had null pointer protection in place. However, there is still a risk of out-of-bounds access during m_children.insert, so additional protection is necessary.
    
    ## Changelog
    - [fixed] Supplement null pointer protection.
    
    ## Test Plan
    1. Open the Tester.
    1. Check whether a null pointer exception will occur when executing ComponentInstance::insertChild.
    
    ## Checklist
    - [x] `pnpm verify` command has passed successfully.
    - [x] No failures occurred in automatic tests on the `CONCURRENT TESTER` page in the tester app.
    - [x] No failures occurred in automatic tests on the `SEQUENTIAL TESTER` page in the tester app.
    - [x] MR does not include a breaking change or a review has been requested from a Huawei reviewer.
    
    
    See merge request: openharmony-sig/ohos_react_native!1687

--- packages/tester/harmony/react_native_openharmony/src/main/cpp/RNOH/ComponentInstance.cpp
+++ packages/tester/harmony/react_native_openharmony/src/main/cpp/RNOH/ComponentInstance.cpp
@@ -26,6 +26,11 @@ ComponentInstance::ComponentInstance(Context ctx)
 void ComponentInstance::insertChild(
     ComponentInstance::Shared childComponentInstance,
     std::size_t index) {
+  if (index > m_children.size()) {
+    LOG(ERROR) << "index out of range, index=" << index
+               << " size=" << m_children.size();
+    return;
+  }
   auto it = m_children.begin() + index;
   childComponentInstance->setParent(shared_from_this());
   onChildInserted(childComponentInstance, index);
