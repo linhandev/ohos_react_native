a50f91c0b9e471bbc15d6134135202c754af5b26
huangyouhua <huangyouhua2@huawei.com>
2025-10-18 20:51:35 +0800

    feat: Support video frame extraction
    Co-authored-by: alvancf<742997447@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1633 merge 0.77.1-rc.1-ohos into 0.77.1-rc.1-ohos
    
    feat: Support video frame extraction
    
    Created-by: alvancf
    Commit-by: alvancf
    Merged-by: huangyouhua
    Description: ## Why are these changes being made?
    Align with Android, display video thumbnail when setting sandbox video path in image component
    
    ## Changelog
    <!-- List user-visible changes in a concise manner. Use one of the following verbs in past tense:
    - added
    - fixed
    - deprecated
    - removed
    - optimized
    Include [breaking] prefix for breaking changes -->
    
    ## Test Plan
    1、Set sandbox path for images
     source={{
              uri: 'file:///data/storage/el2/base/haps/entry/files/test.mp4',
            }}
     2、If sandbox video exists, display video thumbnail       
    ## Checklist
    - [x] `pnpm verify` command has passed successfully.
    - [x] No failures occurred in automatic tests on the `CONCURRENT TESTER` page in the tester app.
    - [x] No failures occurred in automatic tests on the `SEQUENTIAL TESTER` page in the tester app.
    - [x] MR does not include a breaking change or a review has been requested from a Huawei reviewer.
    
    See merge request: openharmony-sig/ohos_react_native!1633

--- packages/tester/harmony/react_native_openharmony/src/main/ets/RNOHCorePackage/turboModules/ImageLoaderTurboModule.ts
+++ packages/tester/harmony/react_native_openharmony/src/main/ets/RNOHCorePackage/turboModules/ImageLoaderTurboModule.ts
@@ -11,28 +11,126 @@ import {
   RemoteImageLoader, RemoteImageMemoryCache
 } from '../../RemoteImageLoader';
 import { UITurboModuleContext } from '../../RNOH/RNOHContext';
+import image from '@ohos.multimedia.image';
+import {fileIo} from '@kit.CoreFileKit';
+import {media} from '@kit.MediaKit';
 
 export class ImageLoaderTurboModule extends UITurboModule {
   static NAME = "ImageLoader" as const
-
   private imageLoader: RemoteImageLoader
+  private videoThumbnailCache: RemoteImageDiskCache
+  private thumbnailDir: string
+  private static readonly VIDEO_FORMATS: Set<string> = new Set([
+    ".mp4", ".mov", ".avi", ".mpeg", ".mpg", ".mkv", ".flv", ".wmv",
+    ".m2ts", ".vob", ".ts", ".webm", ".m3u8", ".mpd", ".3gp", ".3g2",
+    ".rm", ".rmvb", ".f4v", ".mxf"
+  ]);
 
   constructor(protected ctx: UITurboModuleContext) {
     super(ctx)
+    const cacheSize = 128;
     this.imageLoader = new RemoteImageLoader(
-      new RemoteImageMemoryCache(128), new RemoteImageDiskCache(128, `${ctx.uiAbilityContext.cacheDir}/rn_image_cache`),
+      new RemoteImageMemoryCache(cacheSize), new RemoteImageDiskCache(cacheSize, `${ctx.uiAbilityContext.cacheDir}/rn_image_cache`),
       ctx.uiAbilityContext, ({ remoteUri, fileUri }) => {
       ctx.rnInstance.postMessageToCpp('UPDATE_IMAGE_SOURCE_MAP', {
         remoteUri,
         fileUri,
       });
     })
+    this.thumbnailDir = `${ctx.uiAbilityContext.cacheDir}/rn_video_thumbnail_cache`;
+    this.videoThumbnailCache = new RemoteImageDiskCache(cacheSize, this.thumbnailDir);
   }
+
+  protected fetchVideoThumbnail(uri: string): string {
+    if (this.videoThumbnailCache.has(uri)) {
+      return this.getCacheFilePath(uri);
+    } else {
+      this.getVideoThumbnail(uri);
+      return 'pending';
+    }
+  }
+
+  private getCacheFilePath(uri: string): string {
+    return `file://${this.videoThumbnailCache.getLocation(uri)}`;
+  }
+
+  protected async getVideoThumbnail(uri) {
+    try {
+      let fd;
+      const avMetadataExtractor = await media.createAVMetadataExtractor();
+      fd = fileIo.openSync(uri, fileIo.OpenMode.READ_ONLY);
+      avMetadataExtractor.fdSrc = fd;
+
+      const metadata = await avMetadataExtractor.fetchMetadata();
+      avMetadataExtractor.release();
+
+      const avImageGenerator: media.AVImageGenerator = await media.createAVImageGenerator();
+      avImageGenerator.fdSrc = fd;
+
+      const timeUs: number = 0;
+      const queryOption: media.AVImageQueryOptions = media.AVImageQueryOptions.AV_IMAGE_QUERY_NEXT_SYNC;
+      const backUpNum = 100;
+      const param: media.PixelMapParams = {
+        width : Number(metadata.videoWidth) || backUpNum,
+        height : Number(metadata.videoHeight) || backUpNum,
+      };
+      const pixelMap = await avImageGenerator.fetchFrameByTime(
+        timeUs,
+        queryOption,
+        param,
+      );
+      avImageGenerator.release();
+
+      const imagePackerApi: image.ImagePacker = image.createImagePacker();
+      const packOpts: image.PackingOption = {format: 'image/jpeg', quality: 100};
+      if (pixelMap != undefined) {
+        await imagePackerApi
+          .packing(pixelMap, packOpts)
+          .then(async (data: ArrayBuffer) => {
+            const filePath = this.getCacheFilePath(uri);
+            const fd = fileIo.openSync(filePath, fileIo.OpenMode.READ_WRITE | fileIo.OpenMode.CREATE);
+            await fileIo.write(fd.fd, data);
+
+            this.videoThumbnailCache.set(uri);
+            this.ctx.rnInstance.postMessageToCpp('UPDATE_IMAGE_SOURCE_MAP', {
+              remoteUri: uri,
+              fileUri: filePath,
+            });
+          });
+      } else {
+       this.ctx.logger.info('pixelMap is undefined');
+      }
+    } catch (e) {
+      this.ctx.logger.error(JSON.stringify(e));
+    }
+  }
+
+private isVideoUri(uri: string): boolean {
+  if (!uri) {
+    return false;
+  } 
+
+  const paramIndex = uri.indexOf('?');
+  const path = paramIndex === -1 ? uri : uri.substring(0, paramIndex);
+
+  const lastDotIndex = path.lastIndexOf('.');
+  if (lastDotIndex === -1) {
+    return false;
+  }
+
+  const uriSuffix = path.slice(lastDotIndex); 
+  const uriSuffixLower = uriSuffix.toLowerCase();
+  
+  return ImageLoaderTurboModule.VIDEO_FORMATS.has(uriSuffixLower);
+}
 
   /**
    * called from cpp
    */
   protected getPrefetchResult(uri: string): string | undefined {
+    if (this.isVideoUri(uri)) {
+      return this.fetchVideoThumbnail(uri);
+    }
     return this.imageLoader.getPrefetchResult(uri);
   }
 
